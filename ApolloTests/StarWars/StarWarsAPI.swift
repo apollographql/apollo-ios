a6304b6239fad63c90cd2f9c99a3b10b29b9ed00
Martijn Walraven <martijn@martijnwalraven.com>
2016-09-14 17:21:34 +0200

    Remove unused StarWarsAPI directory

--- ApolloTests/StarWars/StarWarsAPI.swift
+++ ApolloTests/StarWars/StarWarsAPI.swift
@@ -1,3 +1,5 @@
+//  This file was automatically generated and should not be edited.
+
 import Apollo
 
 public enum Episode: String {
--- StarWarsAPI/StarWarsAPI/Types.swift
+++
@@ -1,9 +0,0 @@
-import Apollo
-
-public enum Episode: String {
-  case newhope = "NEWHOPE"
-  case empire = "EMPIRE"
-  case jedi = "JEDI"
-}
-
-extension Episode: JSONDecodable, JSONEncodable {}
