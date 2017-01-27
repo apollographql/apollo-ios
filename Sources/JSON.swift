c3c4861fdbb6c65ea823093120a39e9e2e6bf61d
Martijn Walraven <martijn@martijnwalraven.com>
2017-01-26 21:08:43 -0800

    Remove AnyEquatable and rely on AnyObject casting instead

--- Sources/JSON.swift
+++ Sources/JSON.swift
@@ -73,3 +73,12 @@ func cast<T>(_ value: JSONValue) throws -> T {
   }
   return castValue
 }
+
+func equals(_ lhs: Any, _ rhs: Any) -> Bool {
+  if let lhs = lhs as? Reference, let rhs = rhs as? Reference {
+    return lhs == rhs
+  }
+  
+  let lhs = lhs as AnyObject, rhs = rhs as AnyObject
+  return lhs.isEqual(rhs)
+}
