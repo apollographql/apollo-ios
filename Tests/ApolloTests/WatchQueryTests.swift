7de0f641e5775c7cefd431a016452c3740afc049
Martijn Walraven <martijn@martijnwalraven.com>
2017-02-07 17:24:11 +0100

    Revert "Move cacheKeyForObject to ApolloClientDelegate protocol"
    This reverts commit ab56be8572a4ce011b9c2a44c110fe5115d8ae8f.

--- Tests/ApolloTests/WatchQueryTests.swift
+++ Tests/ApolloTests/WatchQueryTests.swift
@@ -207,7 +207,7 @@ class WatchQueryTests: XCTestCase {
     ])
     
     let client = ApolloClient(networkTransport: networkTransport, store: store)
-    client.delegate = clientDelegate(cacheKeyForObject: { $0["id"] })
+    client.cacheKeyForObject = { $0["id"] }
     
     var verifyResult: OperationResultHandler<HeroNameWithIdQuery>
     
