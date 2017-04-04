647884e54d63daefe48764e40b0ae43f8d3d0aec
Paul Kompfner <paul.kompfner@airbnb.com>
2017-04-04 14:35:12 -0700

    Create Example project to test Cocoapods installation

---
+++ Example/ApolloExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApolloExample
+//
+//  Created by paul_kompfner on 4/4/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ Example/Pods/Target Support Files/Apollo/Apollo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Apollo : NSObject
+@end
+@implementation PodsDummy_Apollo
+@end
