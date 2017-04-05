6384fb4c7cc804f1e1215cfa2fd573d07f0ec0de
Paul Kompfner <paul.kompfner@airbnb.com>
2017-04-04 19:25:44 -0700

    Workaround to tests not running due to Apollo.framework not being in framework directory by the time StarWarsAPI generate API build phase is run

--- Example/Pods/Target Support Files/Pods-ApolloTests/Pods-ApolloTests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-ApolloTests/Pods-ApolloTests-frameworks.sh
@@ -87,6 +87,15 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Apollo/Apollo.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SQLite.swift/SQLite.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/Apollo/Apollo.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/SQLite.swift/SQLite.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
