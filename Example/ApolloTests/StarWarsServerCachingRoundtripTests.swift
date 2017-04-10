6d9ef4820d1d05c6f9ee877b9bdcfdbadbca4923
Paul Kompfner <paul.kompfner@airbnb.com>
2017-04-10 16:13:00 -0700

    Update StarWarsServerCachingRoundtripTests to use both in-memory and sqlite caches

--- Example/ApolloTests/StarWarsServerCachingRoundtripTests.swift
+++ Example/ApolloTests/StarWarsServerCachingRoundtripTests.swift
@@ -3,14 +3,7 @@ import XCTest
 import StarWarsAPI
 
 class StarWarsServerCachingRoundtripTests: XCTestCase {
-  var client: ApolloClient!
-  
-  override func setUp() {
-    super.setUp()
-    
-    client = ApolloClient(url: URL(string: "http://localhost:8080/graphql")!)
-  }
-  
+
   func testHeroAndFriendsNamesQuery() {
     let query = HeroAndFriendsNamesQuery()
     
@@ -32,41 +25,50 @@ class StarWarsServerCachingRoundtripTests: XCTestCase {
   }
   
   func testHeroAndFriendsNamesWithIDsQuery() {
-    client.store.cacheKeyForObject = { $0["id"] }
-    
     let query = HeroAndFriendsNamesWithIDsQuery()
     
-    fetchAndLoadFromStore(query: query) { data in
+    fetchAndLoadFromStore(query: query, setupClient: { $0.store.cacheKeyForObject = {$0["id"]} }) { data in
       XCTAssertEqual(data.hero?.name, "R2-D2")
       let friendsNames = data.hero?.friends?.flatMap { $0?.name }
       XCTAssertEqual(friendsNames, ["Luke Skywalker", "Han Solo", "Leia Organa"])
     }
   }
   
-  private func fetchAndLoadFromStore<Query: GraphQLQuery>(query: Query, completionHandler: @escaping (_ data: Query.Data) -> Void) {
-    let expectation = self.expectation(description: "Fetching query")
-    
-    client.fetch(query: query) { (result, error) in
-      if let error = error { XCTFail("Error while fetching query: \(error.localizedDescription)");  return }
-      guard let result = result else { XCTFail("No query result");  return }
-      
-      if let errors = result.errors {
-        XCTFail("Errors in query result: \(errors)")
+  private func fetchAndLoadFromStore<Query: GraphQLQuery>(query: Query, setupClient: ((ApolloClient) -> Void)? = nil, completionHandler: @escaping (_ data: Query.Data) -> Void) {
+
+    withEachCacheType { (cache) in
+      let network = HTTPNetworkTransport(url: URL(string: "http://localhost:8080/graphql")!)
+      let store = ApolloStore(cache: cache)
+      let client = ApolloClient(networkTransport: network, store: store)
+
+      if let setupClient = setupClient {
+        setupClient(client)
       }
-      
-      guard result.data != nil else { XCTFail("No query result data");  return }
-            
-      self.client.store.load(query: query) { (result, error) in
-        defer { expectation.fulfill() }
-        
-        if let error = error { XCTFail("Error while loading query from store: \(error.localizedDescription)");  return }
-        
-        guard let data = result?.data else { XCTFail("No query result data");  return }
-        
-        completionHandler(data)
+
+      let expectation = self.expectation(description: "Fetching query")
+
+      client.fetch(query: query) { (result, error) in
+        if let error = error { XCTFail("Error while fetching query: \(error.localizedDescription)");  return }
+        guard let result = result else { XCTFail("No query result");  return }
+
+        if let errors = result.errors {
+          XCTFail("Errors in query result: \(errors)")
+        }
+
+        guard result.data != nil else { XCTFail("No query result data");  return }
+
+        client.store.load(query: query) { (result, error) in
+          defer { expectation.fulfill() }
+
+          if let error = error { XCTFail("Error while loading query from store: \(error.localizedDescription)");  return }
+
+          guard let data = result?.data else { XCTFail("No query result data");  return }
+
+          completionHandler(data)
+        }
       }
+
+      waitForExpectations(timeout: 1, handler: nil)
     }
-    
-    waitForExpectations(timeout: 1, handler: nil)
   }
 }
