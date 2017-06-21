58068b8fcd68d8bd558a24fc3e2eca4e9c8f2b19
Martijn Walraven <martijn@martijnwalraven.com>
2017-06-21 16:03:48 +0200

    Clean up DataLoader code that worked around an issue fixed in Xcode 8.3

--- Sources/Apollo/DataLoader.swift
+++ Sources/Apollo/DataLoader.swift
@@ -46,11 +46,7 @@ public final class DataLoader<Key: Hashable, Value> {
       let keys = loads.map { $0.key }
       
       self.batchLoad(keys).andThen { values in
-        // TODO: Using zip would have been the nicest solution, but it currently leads to a compiler crash. 
-        // This seems to have been fixed in Xcode 8.3, so we can replace it once that is out.
-        // for (load, value) in zip(loads, values) {
-        for (index, value) in values.enumerated() {
-          let load = loads[index]
+        for (load, value) in zip(loads, values) {
           load.fulfill(value)
         }
       }
