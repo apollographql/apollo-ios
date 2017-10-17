135efeb8157e87b0ded33f69e39e6f7f6aac6ce1
Jeff Seibert <jeff@jseibert.com>
2017-10-16 20:47:06 -0700

    Swift Packager Manager updates

--- Package.swift
+++ Package.swift
@@ -6,7 +6,6 @@ import PackageDescription
 let package = Package(
     name: "Apollo",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "Apollo",
             targets: ["Apollo"]),
@@ -15,8 +14,6 @@ let package = Package(
 
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Apollo",
             dependencies: []),
@@ -34,15 +31,3 @@ let package = Package(
             dependencies: ["ApolloTestSupport", "StarWarsAPI"]),
     ]
 )
-
-
-
-// import PackageDescription
-//
-// let package = Package(
-//     name: "Apollo",
-//     targets: [
-//         Target(name: "Apollo"),
-//     ],
-//     exclude: ["Tests", "Sources/ApolloSQLite"]
-// )
