e7a6f6b624591aa37740f62a6ad2997fc07b2cb8
Jeff Seibert <jeff@jseibert.com>
2017-10-17 13:51:44 -0700

    Swift 4.0 Package Manager support (#164)
    * Swift Packager Manager updates
    
    * Swift Packager Manager updates

--- Package.swift
+++ Package.swift
@@ -1,9 +1,33 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "Apollo",
-    targets: [
-        Target(name: "Apollo"),
+    products: [
+        .library(
+            name: "Apollo",
+            targets: ["Apollo"]),
     ],
-    exclude: ["Tests", "Sources/ApolloSQLite"]
+    dependencies: [
+
+    ],
+    targets: [
+        .target(
+            name: "Apollo",
+            dependencies: []),
+        .testTarget(
+            name: "ApolloTestSupport",
+            dependencies: ["Apollo"]),
+        .testTarget(
+            name: "StarWarsAPI",
+            dependencies: ["Apollo"]),
+        .testTarget(
+            name: "ApolloTests",
+            dependencies: ["ApolloTestSupport", "StarWarsAPI"]),
+        .testTarget(
+            name: "ApolloPerformanceTests",
+            dependencies: ["ApolloTestSupport", "StarWarsAPI"]),
+    ]
 )
--- Tests/ApolloTestSupport/MockNetworkTransport.swift
+++ Tests/ApolloTestSupport/MockNetworkTransport.swift
@@ -1,12 +1,13 @@
 import Apollo
+import Dispatch
 
 public final class MockNetworkTransport: NetworkTransport {
   let body: JSONObject
-  
+
   public init(body: JSONObject) {
     self.body = body
   }
-  
+
   public func send<Operation>(operation: Operation, completionHandler: @escaping (GraphQLResponse<Operation>?, Error?) -> Void) -> Cancellable {
     DispatchQueue.global(qos: .default).async {
       completionHandler(GraphQLResponse(operation: operation, body: self.body), nil)
