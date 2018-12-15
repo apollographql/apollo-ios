40b8186af68a6dccf108ef98c9d82b1a8f204a63
Scott Talbot <stalbot@scentregroup.com>
2018-12-15 18:30:23 +1100

    Update type of GroupedSequenceIterator.keyIterator

--- Sources/Apollo/Collections.swift
+++ Sources/Apollo/Collections.swift
@@ -44,7 +44,7 @@ extension GroupedSequence: Sequence {
 struct GroupedSequenceIterator<Key: Equatable, Value>: IteratorProtocol {
   private var base: GroupedSequence<Key, Value>
   
-  private var keyIterator: EnumeratedIterator<IndexingIterator<Array<Key>>>
+  private var keyIterator: EnumeratedSequence<Array<Key>>.Iterator
   
   init(base: GroupedSequence<Key, Value>) {
     self.base = base
