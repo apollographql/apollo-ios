63884c2a3c10e9459f5015d4167b4a6260907d87
Michael Bachand <michael.bachand@airbnb.com>
2019-01-15 13:39:13 -0800

    Update to LTS version of Node

--- scripts/check-and-run-apollo-cli.sh
+++ scripts/check-and-run-apollo-cli.sh
@@ -1,7 +1,7 @@
 # Only major and minor version should be specified here
 REQUIRED_APOLLO_CLI_VERSION=1.9
-# Specify fully qualified version here
-REQUIRED_NODE_VERSION=8.0.0
+# Specify fully qualified version here. Ideally this should be a LTS version.
+REQUIRED_NODE_VERSION=8.15.0
 
 # Using npx to execute 'apollo' looks for a local install in node_modules before checking $PATH (for a global install)
 APOLLO_CLI="npx --no-install apollo"
