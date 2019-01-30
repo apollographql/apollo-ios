b28c3dcae50c36518bc75036948eda91aa54d6ec
Scott Talbot <stalbot@scentregroup.com>
2019-01-30 16:42:41 +1100

    Update for compatibility with Swift 5 (#427)
    * Remove unused Dictionary defaultable subscript
    
    * Update type of GroupedSequenceIterator.keyIterator

--- Sources/Apollo/Collections.swift
+++ Sources/Apollo/Collections.swift
@@ -1,17 +1,3 @@
-extension Dictionary {
-  subscript(key: Key, withDefault value: @autoclosure () -> Value) -> Value {
-    mutating get {
-      if self[key] == nil {
-        self[key] = value()
-      }
-      return self[key]!
-    }
-    set {
-      self[key] = newValue
-    }
-  }
-}
-
 public extension Dictionary {
   static func += (lhs: inout Dictionary, rhs: Dictionary) {
     #if swift(>=3.2)
@@ -58,7 +44,7 @@ extension GroupedSequence: Sequence {
 struct GroupedSequenceIterator<Key: Equatable, Value>: IteratorProtocol {
   private var base: GroupedSequence<Key, Value>
   
-  private var keyIterator: EnumeratedIterator<IndexingIterator<Array<Key>>>
+  private var keyIterator: EnumeratedSequence<Array<Key>>.Iterator
   
   init(base: GroupedSequence<Key, Value>) {
     self.base = base
