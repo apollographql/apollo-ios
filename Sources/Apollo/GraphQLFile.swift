fe68f3724eab30049ecc9b179bab6e344dc434c6
Kim de Vos <kdv@trifork.com>
2019-07-11 18:34:40 +0200

    Added new file types to support file upload

---
+++ Sources/Apollo/GraphQLFile.swift
@@ -0,0 +1,49 @@
+import Foundation
+
+public struct GraphQLFile {
+  let fieldName: String
+  let originalName: String
+  let mimeType: String
+  let inputStream: InputStream
+  let contentLength: UInt64
+
+  public init(fieldName: String, originalName: String, mimeType: String = "application/octet-stream", data: Data) {
+    self.init(fieldName: fieldName, originalName: originalName, mimeType: mimeType, inputStream: InputStream(data: data), contentLength: UInt64(data.count))
+  }
+
+  public init?(fieldName: String, originalName: String, mimeType: String = "application/octet-stream", fileURL: URL) {
+    // TODO: Better error handling
+
+
+    guard let inputStream = InputStream(url: fileURL) else {
+      return nil
+    }
+
+    guard let contentLength = GraphQLFile.getFileSize(fileURL: fileURL) else {
+      return nil
+    }
+
+    self.init(fieldName: fieldName, originalName: originalName, mimeType: mimeType, inputStream: inputStream, contentLength: contentLength)
+  }
+
+  public init(fieldName: String, originalName: String, mimeType: String = "application/octet-stream", inputStream: InputStream, contentLength: UInt64) {
+    self.fieldName = fieldName
+    self.originalName = originalName
+    self.mimeType = mimeType
+
+    self.inputStream = inputStream
+    self.contentLength = contentLength
+  }
+
+  private static func getFileSize(fileURL: URL) -> UInt64? {
+    do {
+      guard let fileSize = try FileManager.default.attributesOfItem(atPath: fileURL.path)[.size] as? NSNumber else {
+        return nil
+      }
+
+      return fileSize.uint64Value
+    } catch {
+      return nil
+    }
+  }
+}
