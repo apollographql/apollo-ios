6d7af3d775738abbcbf349997f5c4516530ea0fd
Henry Hong <henry.hong@scmp.com>
2019-07-17 18:52:21 +0800

    use url.queryItems to test urlQuery against 2d GraphQLMap in GETTransformer for SDK lower than 11.

--- Tests/ApolloTests/GETTransformerTests.swift
+++ Tests/ApolloTests/GETTransformerTests.swift
@@ -109,9 +109,50 @@ class GETTransformerTests: XCTestCase {
     let transformer = GraphQLGETTransformer(body: body, url: self.url)
     
     let url = transformer.createGetURL()
-    let queryString = url?.absoluteString == "http://localhost:8080/graphql?extensions=%7B%22persistedQuery%22:%7B%22sha256Hash%22:%22f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671%22,%22version%22:1%7D%7D&query=query%20HeroName($episode:%20Episode)%20%7B%0A%20%20hero(episode:%20$episode)%20%7B%0A%20%20%20%20__typename%0A%20%20%20%20name%0A%20%20%7D%0A%7D&variables=%7B%22episode%22:%22EMPIRE%22%7D"
     
-    XCTAssertTrue(queryString)
+    if #available(iOS 11, macOS 13, watchOS 4, tvOS 11, *) {
+      let queryString = url?.absoluteString == "http://localhost:8080/graphql?extensions=%7B%22persistedQuery%22:%7B%22sha256Hash%22:%22f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671%22,%22version%22:1%7D%7D&query=query%20HeroName($episode:%20Episode)%20%7B%0A%20%20hero(episode:%20$episode)%20%7B%0A%20%20%20%20__typename%0A%20%20%20%20name%0A%20%20%7D%0A%7D&variables=%7B%22episode%22:%22EMPIRE%22%7D"
+
+      XCTAssertTrue(queryString)
+    } else {
+      guard let query = url?.queryItems?["query"] else {
+        XCTFail("query should not nil")
+        return
+      }
+      XCTAssertTrue(query == operation.queryDocument)
+      
+      guard let variables = url?.queryItems?["variables"] else {
+        XCTFail("variables should not nil")
+        return
+      }
+      XCTAssert(variables == "{\"episode\":\"EMPIRE\"}")
+      
+      guard let ext = url?.queryItems?["extensions"],
+        let data = ext.data(using: .utf8),
+        let jsonBody = try? JSONSerializationFormat.deserialize(data: data) as? JSONObject
+        else {
+          XCTFail("extensions json data should not be nil")
+          return
+      }
+      
+      guard let comparePersistedQuery = jsonBody["persistedQuery"] as? JSONObject else {
+        XCTFail("persistedQuery is missing")
+        return
+      }
+      
+      guard let sha256Hash = comparePersistedQuery["sha256Hash"] as? String else {
+        XCTFail("sha256Hash is missing")
+        return
+      }
+      
+      guard let version = comparePersistedQuery["version"] as? Int else {
+        XCTFail("version is missing")
+        return
+      }
+      
+      XCTAssert(version == 1)
+      XCTAssert(sha256Hash == "f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671")
+    }
   }
   
   func testEncodingQueryWith2DWOQueryParameter() {
@@ -134,9 +175,44 @@ class GETTransformerTests: XCTestCase {
     let transformer = GraphQLGETTransformer(body: body, url: self.url)
     
     let url = transformer.createGetURL()
-    let queryString = url?.absoluteString == "http://localhost:8080/graphql?extensions=%7B%22persistedQuery%22:%7B%22sha256Hash%22:%22f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671%22,%22version%22:1%7D%7D&variables=%7B%22episode%22:%22EMPIRE%22%7D"
     
-    XCTAssertTrue(queryString)
+    if #available(iOS 11, macOS 13, watchOS 4, tvOS 11, *) {
+      let queryString = url?.absoluteString == "http://localhost:8080/graphql?extensions=%7B%22persistedQuery%22:%7B%22sha256Hash%22:%22f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671%22,%22version%22:1%7D%7D&variables=%7B%22episode%22:%22EMPIRE%22%7D"
+      XCTAssertTrue(queryString)
+    } else {
+
+      guard let variables = url?.queryItems?["variables"] else {
+        XCTFail("variables should not nil")
+        return
+      }
+      XCTAssert(variables == "{\"episode\":\"EMPIRE\"}")
+      
+      guard let ext = url?.queryItems?["extensions"],
+        let data = ext.data(using: .utf8),
+        let jsonBody = try? JSONSerializationFormat.deserialize(data: data) as? JSONObject
+        else {
+          XCTFail("extensions json data should not be nil")
+          return
+      }
+      
+      guard let comparePersistedQuery = jsonBody["persistedQuery"] as? JSONObject else {
+        XCTFail("persistedQuery is missing")
+        return
+      }
+      
+      guard let sha256Hash = comparePersistedQuery["sha256Hash"] as? String else {
+        XCTFail("sha256Hash is missing")
+        return
+      }
+      
+      guard let version = comparePersistedQuery["version"] as? Int else {
+        XCTFail("version is missing")
+        return
+      }
+      
+      XCTAssert(version == 1)
+      XCTAssert(sha256Hash == "f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671")
+    }
   }
   
   func testEncodingQueryWithNullDefaultParameter() {
@@ -175,9 +251,43 @@ class GETTransformerTests: XCTestCase {
     let transformer = GraphQLGETTransformer(body: body, url: self.url)
     
     let url = transformer.createGetURL()
-    let queryString = url?.absoluteString == "http://localhost:8080/graphql?extensions=%7B%22persistedQuery%22:%7B%22sha256Hash%22:%22f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671%22,%22version%22:1%7D%7D&query=query%20HeroName($episode:%20Episode)%20%7B%0A%20%20hero(episode:%20$episode)%20%7B%0A%20%20%20%20__typename%0A%20%20%20%20name%0A%20%20%7D%0A%7D&variables=%7B%22episode%22:null%7D"
     
-    XCTAssertTrue(queryString)
+    if #available(iOS 11, macOS 13, watchOS 4, tvOS 11, *) {
+    let queryString = url?.absoluteString == "http://localhost:8080/graphql?extensions=%7B%22persistedQuery%22:%7B%22sha256Hash%22:%22f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671%22,%22version%22:1%7D%7D&query=query%20HeroName($episode:%20Episode)%20%7B%0A%20%20hero(episode:%20$episode)%20%7B%0A%20%20%20%20__typename%0A%20%20%20%20name%0A%20%20%7D%0A%7D&variables=%7B%22episode%22:null%7D"
+      XCTAssertTrue(queryString)
+    } else {
+      guard let variables = url?.queryItems?["variables"] else {
+        XCTFail("variables should not nil")
+        return
+      }
+      XCTAssert(variables == "{\"episode\":null}")
+      
+      guard let ext = url?.queryItems?["extensions"],
+        let data = ext.data(using: .utf8),
+        let jsonBody = try? JSONSerializationFormat.deserialize(data: data) as? JSONObject
+        else {
+          XCTFail("extensions json data should not be nil")
+          return
+      }
+      
+      guard let comparePersistedQuery = jsonBody["persistedQuery"] as? JSONObject else {
+        XCTFail("persistedQuery is missing")
+        return
+      }
+      
+      guard let sha256Hash = comparePersistedQuery["sha256Hash"] as? String else {
+        XCTFail("sha256Hash is missing")
+        return
+      }
+      
+      guard let version = comparePersistedQuery["version"] as? Int else {
+        XCTFail("version is missing")
+        return
+      }
+      
+      XCTAssert(version == 1)
+      XCTAssert(sha256Hash == "f6e76545cd03aa21368d9969cb39447f6e836a16717823281803778e7805d671")
+    }
   }
   
   func testMissingQueryParameterInBodyReturnsNil() {
