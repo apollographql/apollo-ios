0c059b4a87d6873be32802544df79ce5c8365c91
Ellen Shapiro <designatednerd@gmail.com>
2019-07-17 16:34:08 +0200

    pull out split network transport's `NetworkTransport` conformance; update docs

--- Sources/ApolloWebSocket/SplitNetworkTransport.swift
+++ Sources/ApolloWebSocket/SplitNetworkTransport.swift
@@ -2,21 +2,31 @@
 import Apollo
 #endif
 
-
-public class SplitNetworkTransport: NetworkTransport {
+/// A network transport that sends subscriptions using one network transport and other requests using another `NetworkTransport`.
+public class SplitNetworkTransport {
   private let httpNetworkTransport: NetworkTransport
   private let webSocketNetworkTransport: NetworkTransport
   
+  /// Designated initializer
+  ///
+  /// - Parameters:
+  ///   - httpNetworkTransport: A `NetworkTransport` to use for non-subscription requests. Should generally be a `HTTPNetworkTransport` or something similar.
+  ///   - webSocketNetworkTransport: A `NetworkTransport` to use for subscription requests. Should generally be a `WebSocketTransport` or something similar.
   public init(httpNetworkTransport: NetworkTransport, webSocketNetworkTransport: NetworkTransport) {
     self.httpNetworkTransport = httpNetworkTransport
     self.webSocketNetworkTransport = webSocketNetworkTransport
   }
+}
+
+// MARK: - NetworkTransport conformance
+
+extension SplitNetworkTransport: NetworkTransport {
   
   public func send<Operation>(operation: Operation, completionHandler: @escaping (Result<GraphQLResponse<Operation>, Error>) -> Void) -> Cancellable {
     if operation.operationType == .subscription {
-        return webSocketNetworkTransport.send(operation: operation, completionHandler: completionHandler)
+      return webSocketNetworkTransport.send(operation: operation, completionHandler: completionHandler)
     } else {
-        return httpNetworkTransport.send(operation: operation, completionHandler: completionHandler)
+      return httpNetworkTransport.send(operation: operation, completionHandler: completionHandler)
     }
   }
 }
