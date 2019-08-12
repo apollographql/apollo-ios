6b6b859aa49b0ff0e2efb4255a7e2ac90a7fa4c0
Ellen Shapiro <designatednerd@gmail.com>
2019-08-12 22:06:09 +0200

    update in-memory normalized cache to use callbacks

--- Sources/Apollo/InMemoryNormalizedCache.swift
+++ Sources/Apollo/InMemoryNormalizedCache.swift
@@ -4,18 +4,48 @@ public final class InMemoryNormalizedCache: NormalizedCache {
   public init(records: RecordSet = RecordSet()) {
     self.records = records
   }
-
-  public func loadRecords(forKeys keys: [CacheKey]) -> Promise<[Record?]> {
+  
+  public func loadRecords(forKeys keys: [CacheKey],
+                          callbackQueue: DispatchQueue?,
+                          completion: @escaping (Result<[Record?], Error>) -> Void) {
     let records = keys.map { self.records[$0] }
-    return Promise(fulfilled: records)
+    if let callbackQueue = callbackQueue {
+      callbackQueue.async {
+        completion(.success(records))
+      }
+    } else {
+      completion(.success(records))
+    }
+  }
+  
+  public func merge(records: RecordSet,
+                    callbackQueue: DispatchQueue?,
+                    completion: @escaping (Result<Set<CacheKey>, Error>) -> Void) {
+    let cacheKeys = self.records.merge(records: records)
+    
+    if let callbackQueue = callbackQueue {
+      callbackQueue.async {
+        completion(.success(cacheKeys))
+      }
+    } else {
+      completion(.success(cacheKeys))
+    }
   }
 
-  public func merge(records: RecordSet) -> Promise<Set<CacheKey>> {
-    return Promise(fulfilled: self.records.merge(records: records))
-  }
-
-  public func clear() -> Promise<Void> {
-    records.clear()
-    return Promise(fulfilled: ())
+  public func clear(callbackQueue: DispatchQueue?,
+                    completion: ((Result<Void, Error>) -> Void)?) {
+    self.records.clear()
+    
+    guard let completion = completion else {
+      return
+    }
+    
+    if let callbackQueue = callbackQueue {
+      callbackQueue.async {
+        completion(.success(()))
+      }
+    } else {
+      completion(.success(()))
+    }
   }
 }
