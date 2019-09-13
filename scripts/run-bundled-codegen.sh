363b07e404c8f1f7251bef33030aea663a8fae53
Ellen Shapiro <designatednerd@gmail.com>
2019-09-12 23:19:03 -0500

    add a script to run bundled codegen from the `scripts` directory

---
+++ scripts/run-bundled-codegen.sh
@@ -0,0 +1,9 @@
+# Get the path to the script directory
+SCRIPT_DIR="$(eval dirname $0)"
+
+# Use the bundled executable of the Apollo CLI to generate code
+APOLLO_CLI="${SCRIPT_DIR}/apollo/bin/run"
+
+# Print commands before executing them (useful for troubleshooting)
+set -x
+$APOLLO_CLI "$@"
