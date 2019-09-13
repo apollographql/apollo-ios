ff29938df7a678349b1729b533f7317090a352c6
Ellen Shapiro <designatednerd@gmail.com>
2019-09-13 16:27:41 -0500

    log barf some info about the CLI when running it directly so we know what version people are using

--- scripts/run-bundled-codegen.sh
+++ scripts/run-bundled-codegen.sh
@@ -4,6 +4,9 @@ SCRIPT_DIR="$(eval dirname $0)"
 # Use the bundled executable of the Apollo CLI to generate code
 APOLLO_CLI="${SCRIPT_DIR}/apollo/bin/run"
 
+# Print version
+echo "Apollo CLI Information: $(eval ${APOLLO_CLI} --version)"
+
 # Print commands before executing them (useful for troubleshooting)
 set -x
 $APOLLO_CLI "$@"
