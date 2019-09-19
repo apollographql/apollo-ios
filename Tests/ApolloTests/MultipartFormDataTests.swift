2827e0098e57868580d1c1cbff2500445d14d5d2
Ellen Shapiro <designatednerd@gmail.com>
2019-09-18 21:38:18 -0500

    update tests to account for multiline strings

--- Tests/ApolloTests/MultipartFormDataTests.swift
+++ Tests/ApolloTests/MultipartFormDataTests.swift
@@ -180,7 +180,7 @@ Charlie file content.
 --TEST.BOUNDARY
 Content-Disposition: form-data; name="operations"
 
-{"operationName":"HeroName","query":"query HeroName($episode: Episode) { hero(episode: $episode) { __typename name } }","variables":{"episode":null,\"upload\":null}}
+{"operationName":"HeroName","query":"query HeroName($episode: Episode) {\\n  hero(episode: $episode) {\\n    __typename\\n    name\\n  }\\n}","variables":{"episode":null,"upload":null}}
 --TEST.BOUNDARY
 Content-Disposition: form-data; name="map"
 
@@ -242,7 +242,7 @@ Alpha file content.
 --TEST.BOUNDARY
 Content-Disposition: form-data; name="operations"
 
-{"operationName":"HeroName","query":"query HeroName($episode: Episode) { hero(episode: $episode) { __typename name } }","variables":{"episode":null,\"uploads\":null}}
+{"operationName":"HeroName","query":"query HeroName($episode: Episode) {\\n  hero(episode: $episode) {\\n    __typename\\n    name\\n  }\\n}","variables":{"episode":null,"uploads":null}}
 --TEST.BOUNDARY
 Content-Disposition: form-data; name="map"
 
