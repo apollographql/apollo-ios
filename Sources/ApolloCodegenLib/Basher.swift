6322b2451abf26700f9ac78852947821d2695463
Ellen Shapiro <designatednerd@gmail.com>
2019-10-08 20:27:10 -0500

    fix alignment and error output for basher

--- Sources/ApolloCodegenLib/Basher.swift
+++ Sources/ApolloCodegenLib/Basher.swift
@@ -13,53 +13,52 @@ public struct Basher {
   public enum BashError: Error, LocalizedError {
     case errorDuringTask(errorString: String, code: Int32)
     case noOutput(code: Int32)
-
-    public var localizedDescription: String {
+    
+    public var errorDescription: String? {
       switch self {
-        case .errorDuringTask(let errorString, let code):
-          return "Task failed with code \(code): \(errorString)"
-        case .noOutput(let code):
-          return "Task had no output. Exit code: \(code)."
+      case .errorDuringTask(let errorString, let code):
+        return "Task failed with code \(code). Output: \(errorString)"
+      case .noOutput(let code):
+        return "Task had no output. Exit code: \(code)."
       }
     }
   }
   
   public static func run(command: String, from url: URL?) throws -> String {
-      let task = Process()
-      let pipe = Pipe()
-      task.standardOutput = pipe
-      task.standardError = pipe
-      task.arguments = [
-        "-c",
-        command        
-      ]
-      task.launchPath = "/bin/bash"
-
-      if #available(OSX 10.13, *) {
-        if let url = url {
-          task.currentDirectoryURL = url
-        }
-        try task.run()
-      } else {
-        if let path = url?.path {
-          task.currentDirectoryPath = path
-        }
-        task.launch()
-      }
+    let task = Process()
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = [
+      "-c",
+      command
+    ]
+    task.launchPath = "/bin/bash"
     
-      task.waitUntilExit()
-      
-      let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    if #available(OSX 10.13, *) {
+      if let url = url {
+        task.currentDirectoryURL = url
+      }
+      try task.run()
+    } else {
+      if let path = url?.path {
+        task.currentDirectoryPath = path
+      }
+      task.launch()
+    }
     
-      guard let output = String(bytes: data, encoding: .utf8) else {
-        throw BashError.noOutput(code: task.terminationStatus)
-      }
-      
-      
-      guard task.terminationStatus == 0 else {
-        throw BashError.errorDuringTask(errorString: output, code: task.terminationStatus)
-      }
-      
-      return output
+    task.waitUntilExit()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    
+    guard let output = String(bytes: data, encoding: .utf8) else {
+      throw BashError.noOutput(code: task.terminationStatus)
+    }
+    
+    guard task.terminationStatus == 0 else {
+      throw BashError.errorDuringTask(errorString: output, code: task.terminationStatus)
+    }
+    
+    return output
   }
 }
