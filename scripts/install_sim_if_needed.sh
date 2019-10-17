241c2e8d2739f4fd77d13f0e47bcf600eb112b91
Ellen Shapiro <designatednerd@gmail.com>
2019-10-17 17:12:25 -0500

    pull sim installer out into its own script to see if it will coooperate

---
+++ scripts/install_sim_if_needed.sh
@@ -0,0 +1,6 @@
+if [[ -n "${DEVICE_TYPE}" ]]; then
+  echo "Making sure simulator is installed"
+  xcrun simctl create "${DEVICE_TYPE}_${RUNTIME}" "${DEVICE_TYPE}" "${RUNTIME}"
+else
+  echo "Not installing simulator as DEVICE_TYPE has no content"
+fi
