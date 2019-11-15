f436cbd0b89e8b4fc68ad2521b7db245e28225be
Michael Gray <michael.gray@1stdibs.com>
2019-11-15 12:51:11 -0500

    fixing some swift variants

--- Sources/Apollo/GraphQLHTTPResponseError.swift
+++ Sources/Apollo/GraphQLHTTPResponseError.swift
@@ -41,8 +41,9 @@ public struct GraphQLHTTPResponseError: Error, LocalizedError {
   public var graphQLErrors: [GraphQLError]? {
     guard
       let data = self.body,
-      let json = try? self.serializationFormat.deserialize(data: data) as? JSONObject,
-      let errorArray = json?["errors"] as? [JSONObject] else {
+      let deserializedData = try? self.serializationFormat.deserialize(data: data),
+      let json = deserializedData as? JSONObject,
+      let errorArray = json["errors"] as? [JSONObject] else {
         return nil
     }
     
