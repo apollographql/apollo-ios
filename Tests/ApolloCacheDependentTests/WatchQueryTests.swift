338f14a257fbd166b4dcb18590a7762bafef62ad
Gil Birman <gil.birman@airbnb.com>
2019-11-22 15:30:57 -0800

    Add List-related watch tests

--- Tests/ApolloCacheDependentTests/WatchQueryTests.swift
+++ Tests/ApolloCacheDependentTests/WatchQueryTests.swift
@@ -149,11 +149,101 @@ class WatchQueryTests: XCTestCase {
       expectation = self.expectation(description: "Updated after fetching other query")
 
       client.fetch(query: HeroNameQuery(), cachePolicy: .fetchIgnoringCacheData)
-      
+
       waitForExpectations(timeout: 5, handler: nil)
     }
   }
-  
+
+  func testWatchedQueryGetsUpdatedWithListReorderingFromOtherQuery() throws {
+    let initialRecords: RecordSet = [
+      "QUERY_ROOT": ["hero": Reference(key: "QUERY_ROOT.hero")],
+      "QUERY_ROOT.hero": [
+        "name": "R2-D2",
+        "__typename": "Droid",
+        "friends": [
+          Reference(key: "QUERY_ROOT.hero.friends.0"),
+          Reference(key: "QUERY_ROOT.hero.friends.1"),
+          Reference(key: "QUERY_ROOT.hero.friends.2")
+        ]
+      ],
+      "QUERY_ROOT.hero.friends.0": ["__typename": "Human", "name": "Luke Skywalker"],
+      "QUERY_ROOT.hero.friends.1": ["__typename": "Human", "name": "Han Solo"],
+      "QUERY_ROOT.hero.friends.2": ["__typename": "Human", "name": "Leia Organa"],
+      ]
+
+    withCache(initialRecords: initialRecords) { (cache) in
+      let networkTransport = MockNetworkTransport(body: [
+        "data": [
+          "hero": [
+            "name": "Artoo",
+            "__typename": "Droid",
+            "friends": [
+              ["__typename": "Human", "name": "Luke Skywalker"],
+              ["__typename": "Human", "name": "Leia Organa"],
+            ]
+          ]
+        ]
+        ])
+      let store = ApolloStore(cache: cache)
+      let client = ApolloClient(networkTransport: networkTransport, store: store)
+
+      let query = HeroAndFriendsNamesQuery()
+
+      var verifyResult: GraphQLResultHandler<HeroAndFriendsNamesQuery.Data>
+
+      verifyResult = { result in
+        switch result {
+        case .success(let graphQLResult):
+          XCTAssertNil(graphQLResult.errors)
+          guard let data = graphQLResult.data else {
+            XCTFail("No data in graphQLResult!")
+            return
+          }
+
+          XCTAssertEqual(data.hero?.name, "R2-D2")
+          let friendsNames = data.hero?.friends?.compactMap { $0?.name }
+          XCTAssertEqual(friendsNames, ["Luke Skywalker", "Han Solo", "Leia Organa"])
+        case .failure(let error):
+          XCTFail("Unexpected error: \(error)")
+        }
+      }
+
+      var expectation = self.expectation(description: "Fetching query")
+
+      _ = client.watch(query: query) { result in
+        verifyResult(result)
+        expectation.fulfill()
+      }
+
+      waitForExpectations(timeout: 5, handler: nil)
+
+      verifyResult = { result in
+        switch result {
+        case .success(let graphQLResult):
+          XCTAssertNil(graphQLResult.errors)
+          guard let data = graphQLResult.data else {
+            XCTFail("No data in GraphQL result!")
+            return
+          }
+          XCTAssertEqual(data.hero?.name, "Artoo")
+          let friendsNames = data.hero?.friends?.compactMap { $0?.name }
+          XCTAssertEqual(friendsNames, [
+            "Luke Skywalker",
+            "Leia Organa",
+          ])
+        case .failure(let error):
+          XCTFail("Unexpected error: \(error)")
+        }
+      }
+
+      expectation = self.expectation(description: "Updated after fetching other query")
+
+      client.fetch(query: HeroAndFriendsNamesQuery(), cachePolicy: .fetchIgnoringCacheData)
+
+      waitForExpectations(timeout: 5, handler: nil)
+    }
+  }
+
   func testWatchedQueryDoesNotRefetchAfterUnrelatedQuery() throws {
     let initialRecords: RecordSet = [
       "QUERY_ROOT": ["hero": Reference(key: "QUERY_ROOT.hero")],
@@ -295,6 +385,95 @@ class WatchQueryTests: XCTestCase {
       waitForExpectations(timeout: 5, handler: nil)
     }
   }
+
+  func testWatchedListModifyingQueryWithID() throws {
+    let query = HeroAndFriendsNamesWithIDsQuery()
+    let initialRecords: RecordSet = [
+      "QUERY_ROOT": ["hero": Reference(key: "2001")],
+      "2001": [
+        "id": "2001",
+        "name": "R2-D2",
+        "__typename": "Droid",
+        "friends": [
+          Reference(key: "LS"),
+          Reference(key: "HS"),
+          Reference(key: "LO"),
+        ],
+      ],
+      "LS": ["__typename": "Human", "id": "LS", "name": "Luke Skywalker"],
+      "HS": ["__typename": "Human", "id": "HS", "name": "Han Solo"],
+      "LO": ["__typename": "Human", "id": "LO", "name": "Leia Organa"],
+    ]
+
+    withCache(initialRecords: initialRecords) { (cache) in
+      let networkTransport = MockNetworkTransport(body: [
+        "data": [
+          "hero": [
+            "id": "2001",
+            "name": "Luke Skywalker",
+            "__typename": "Human",
+            "friends": [
+              ["__typename": "Human", "id": "LO"],
+              ["__typename": "Human", "id": "LS"],
+            ]
+          ]
+        ]
+        ])
+      let store = ApolloStore(cache: cache)
+      let client = ApolloClient(networkTransport: networkTransport, store: store)
+      client.store.cacheKeyForObject = { $0["id"] }
+
+      var verifyResult: GraphQLResultHandler<HeroAndFriendsNamesWithIDsQuery.Data>
+
+      verifyResult = { result in
+        switch result {
+        case .success(let graphQLResult):
+          XCTAssertNil(graphQLResult.errors)
+          XCTAssertEqual(graphQLResult.data?.hero?.name, "R2-D2")
+          let friendsNames = graphQLResult.data?.hero?.friends?.compactMap { $0?.name }
+          XCTAssertEqual(friendsNames, [
+            "Luke Skywalker",
+            "Han Solo",
+            "Leia Organa",
+          ])
+        case .failure(let error):
+          XCTFail("Unexpected error: \(error)")
+        }
+      }
+
+      var expectation = self.expectation(description: "Fetching query")
+
+      _ = client.watch(query: query, cachePolicy: .returnCacheDataDontFetch) { result in
+        verifyResult(result)
+        expectation.fulfill()
+      }
+
+      waitForExpectations(timeout: 5, handler: nil)
+
+      verifyResult = { result in
+        switch result {
+        case .success(let graphQLResult):
+          XCTAssertNil(graphQLResult.errors)
+          XCTAssertEqual(graphQLResult.data?.hero?.name, "Luke Skywalker")
+
+          let friendsNames = graphQLResult.data?.hero?.friends?.compactMap { $0?.name }
+          XCTAssertEqual(friendsNames, [
+            "Leia Organa",
+            "Luke Skywalker",
+          ])
+        case .failure(let error):
+          XCTFail("Unexpected error: \(error)")
+        }
+      }
+
+      expectation = self.expectation(description: "Fetching other query")
+
+      client.fetch(query: HeroAndFriendsIDsQuery(), cachePolicy: .fetchIgnoringCacheData)
+
+      waitForExpectations(timeout: 5, handler: nil)
+    }
+  }
+
 
   func testWatchedQueryGetsUpdatedWithResultFromReadWriteTransaction() throws {
     let initialRecords: RecordSet = [
