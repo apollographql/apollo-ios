b7873ce4623fd724b4bad142d43ba8b9468503a8
Ellen Shapiro <designatednerd@gmail.com>
2019-11-28 18:59:29 +0100

    Fix sidebar

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -25,9 +25,10 @@ module.exports = {
             'api-reference'
           ],
           Tutorial: [
-            'tutorial/tutorial_0',
-            'tutorial/tutorial_1',
-            'tutorial/tutorial_2'
+            'tutorial/tutorial-introduction',
+            'tutorial/tutorial-create-project',
+            'tutorial/tutorial-obtain-schema',
+            'tutorial/tutorial-execute-query'
           ],
           Usage:[
             'downloading-schema',
