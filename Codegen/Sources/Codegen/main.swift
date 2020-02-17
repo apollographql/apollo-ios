cc55b25e0d6b0633fddea5a29167aa27b3c4e10e
Ellen Shapiro <designatednerd@gmail.com>
2020-02-17 16:47:42 -0600

    Use the file finder instead of a magic sourceroot var

--- Codegen/Sources/Codegen/main.swift
+++ Codegen/Sources/Codegen/main.swift
@@ -3,27 +3,14 @@ import ApolloCodegenLib
 import TSCUtility
 
 enum MyCodegenError: Error {
-  case sourceRootNotProvided
-  case sourceRootNotADirectory
   case targetDoesntExist(atURL: Foundation.URL)
 }
 
-// This gets passed in automatically when run via an Xcode Build Phase Run
-// Script, but you should set it up in a local (ie, not shared) scheme for
-// your machine in the .xcodeproj created by opening the `Package.swift`
-// for the Codegen project.
-guard let sourceRootPath = ProcessInfo.processInfo.environment["SRCROOT"] else {
-  throw MyCodegenError.sourceRootNotProvided
-}
-
-// Extra check that we've actually got a folder. This part is optional, it
-// just helps figure out what might be going wrong.
-guard FileManager.default.apollo_folderExists(at: sourceRootPath) else {
-  throw MyCodegenError.sourceRootNotADirectory
-}
-
-// This needs to be a URL rather than a path, so we convert it
-let sourceRootURL = URL(fileURLWithPath: sourceRootPath)
+let parentFolderOfScriptFile = FileFinder.findParentFolder()
+let sourceRootURL = parentFolderOfScriptFile
+    .deletingLastPathComponent() // Sources
+    .deletingLastPathComponent() // Codegen
+    .deletingLastPathComponent() // apollo-ios
 
 // In a typical app, you'll only need to do this for one target, so you'd
 // set these up directly within this file. Here, we're using more than one
