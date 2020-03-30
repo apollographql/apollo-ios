6c3242262fb2250109925c6037af819b7305f03f
Ellen Shapiro <designatednerd@gmail.com>
2020-03-30 17:03:49 -0500

    Update download link and SHASUM for version 2.25.0 of the CLI

--- Sources/ApolloCodegenLib/CLIExtractor.swift
+++ Sources/ApolloCodegenLib/CLIExtractor.swift
@@ -25,7 +25,7 @@ struct CLIExtractor {
     }
   }
   
-  static let expectedSHASUM = "bf98280b7164fbb2cd6fa04a9e7869d59798b9717ebee7650c365b920f566c59"
+  static let expectedSHASUM = "5224c3788d0a131483bd1ed640444e0b1becf3ab33e9ce4aef6b0579bd4d7712"
   
   /// Checks to see if the CLI has already been extracted and is the correct version, and extracts or re-extracts as necessary
   ///
