c10314b8f1eb3443c4ea26326e6ee01d7f91d425
Simon Rice <im@simonrice.com>
2020-04-28 18:55:47 +0100

    Add watchOS 3.0 availability for urlSession(:task:didFinishCollecting)

--- Sources/Apollo/URLSessionClient.swift
+++ Sources/Apollo/URLSessionClient.swift
@@ -109,7 +109,7 @@ open class URLSessionClient: NSObject, URLSessionDelegate, URLSessionTaskDelegat
     self.clearAllTasks()
   }
   
-  @available(OSX 10.12, iOS 10.0, tvOS 10.0, *)
+  @available(OSX 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *)
   open func urlSession(_ session: URLSession,
                        task: URLSessionTask,
                        didFinishCollecting metrics: URLSessionTaskMetrics) {
