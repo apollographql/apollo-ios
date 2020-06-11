37e0646a9c05bd9370ffcc447505242db9423148
Ellen Shapiro <designatednerd@gmail.com>
2020-06-11 18:40:16 -0500

    update bundle extension to use apollo namespace

--- Sources/Apollo/Bundle+Helpers.swift
+++ Sources/Apollo/Bundle+Helpers.swift
@@ -1,13 +1,16 @@
 import Foundation
+import ApolloCore
 
-extension Bundle {
+extension Bundle: ApolloCompatible {}
+
+extension ApolloExtension where Base == Bundle {
 
   /// Type-safe getter for info dictionary key objects
   ///
   /// - Parameter key: The key to try to grab an object for
   /// - Returns: The object of the desired type, or nil if it is not present or of the incorrect type.
   func bundleValue<T>(forKey key: String) -> T? {
-    return object(forInfoDictionaryKey: key) as? T
+    return base.object(forInfoDictionaryKey: key) as? T
   }
 
   /// The bundle identifier of this bundle, or nil if not present.
