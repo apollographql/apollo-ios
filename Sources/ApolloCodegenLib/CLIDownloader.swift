eb54eda17b3c5d91757e484a914ff0de93a612ca
Ellen Shapiro <designatednerd@gmail.com>
2020-07-16 16:38:32 -0500

    update to v2.30.1 of JS CLI

--- Sources/ApolloCodegenLib/CLIDownloader.swift
+++ Sources/ApolloCodegenLib/CLIDownloader.swift
@@ -30,7 +30,7 @@ struct CLIDownloader {
   }
   
   /// The URL string for getting the current version of the CLI
-  static let downloadURLString = "https://install.apollographql.com/legacy-cli/darwin/2.28.3"
+  static let downloadURLString = "https://install.apollographql.com/legacy-cli/darwin/2.30.1"
   
   /// Downloads the appropriate Apollo CLI in a zip file.
   ///
