112869ba06371343ae2c8300fc2bf18fb7f88751
Ellen Shapiro <designatednerd@gmail.com>
2020-08-12 19:41:13 -0500

    Remove now-unnecessary custom XCTAssertEqual for arrays

--- Sources/ApolloTestSupport/XCTAssertHelpers.swift
+++ Sources/ApolloTestSupport/XCTAssertHelpers.swift
@@ -1,25 +1,6 @@
 import XCTest
 @testable import Apollo
 
-public func XCTAssertEqual<T: Equatable>(_ expression1: @autoclosure () throws -> [T?]?, _ expression2: @autoclosure () throws -> [T?]?, file: StaticString = #file, line: UInt = #line) rethrows {
-  let optionalValue1 = try expression1()
-  let optionalValue2 = try expression2()
-  
-  let message = {
-    "(\"\(String(describing: optionalValue1))\") is not equal to (\"\(String(describing: optionalValue2))\")"
-  }
-  
-  switch (optionalValue1, optionalValue2) {
-  case (.none, .none):
-    break
-  case let (value1?, value2?):
-    // FIXME: This ignores nil values in both lists, which is probably not what you want for true equality checking
-    XCTAssertEqual(value1.compactMap { $0 }, value2.compactMap { $0 }, message(), file: file, line: line)
-  default:
-    XCTFail(message(), file: file, line: line)
-  }
-}
-
 public func XCTAssertEqual<T, U>(_ expression1: @autoclosure () throws -> [T : U]?, _ expression2: @autoclosure () throws -> [T : U]?, file: StaticString = #file, line: UInt = #line) rethrows {
   let optionalValue1 = try expression1()
   let optionalValue2 = try expression2()
