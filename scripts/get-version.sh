df9751425c6b4ce3a6fb363d87bbeb7a340adef5
Ellen Shapiro <designatednerd@gmail.com>
2020-09-18 12:55:47 -0500

    update version to rc.1

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -4,4 +4,4 @@ source "$(dirname "$0")/version-constants.sh"
 
 prefix="$VERSION_CONFIG_VAR = "
 version_config=$(cat $VERSION_CONFIG_FILE)
-echo "${version_config:${#prefix}}-beta2"
+echo "${version_config:${#prefix}}-rc.1"
