5df4e44eb505f55f53ffcf4261071977c6773807
Nathan Harris <nathan.harris@peek.com>
2020-11-06 20:15:30 -0800

    Change migration unit test to use snapshot files

--- Tests/ApolloSQLiteTests/CachePersistenceTests.swift
+++ Tests/ApolloSQLiteTests/CachePersistenceTests.swift
@@ -9,85 +9,39 @@ import SQLite
 class CachePersistenceTests: XCTestCase {
 
   func testDatabaseSetup() throws {
-    // Create a database with a state corresponding to the first schema version.
-    let sqliteFileURL = SQLiteTestCacheProvider.temporarySQLiteFileURL()
-    let db = try Connection(.uri(sqliteFileURL.absoluteString), readonly: false)
-    try db.run("""
-    CREATE TABLE IF NOT EXISTS "records" ("_id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "key" TEXT NOT NULL UNIQUE, "record" TEXT NOT NULL)
-    """)
-    try db.run("""
-    CREATE UNIQUE INDEX IF NOT EXISTS "index_records_on_key" ON "records" ("key")
-    """)
+    // loop through each of the database snapshots to run through migrations
+    // if a migration fails, then it will throw an error
+    // we verify the migration is successful by comparing the iteration to the schema version (assigned after the migration)
+    let testBundle = Bundle(for: Self.self)
+    try testBundle.paths(forResourcesOfType: "sqlite3", inDirectory: nil)
+      .sorted() // make sure they run in order
+      .map(URL.init(fileURLWithPath:))
+      .enumerated()
+      .forEach { previousSchemaVersion, fileURL in
+        guard FileManager.default.fileExists(atPath: fileURL.path) else {
+          XCTFail("expected snapshot file '\(fileURL.lastPathComponent)' could not be found")
+          return
+        }
 
-    // Add en entry
-    try db.run("""
-    INSERT OR REPLACE INTO "records" ("key", "record") VALUES ("QUERY_ROOT.hero", "{""name"":""Luke Skywalker"",""__typename"":""Human""}")
-    """)
+        // open a connection to the snapshot that is expected to be migrated to the next version
+        try SQLiteTestCacheProvider.withCache(fileURL: fileURL) { cache in
+          guard let sqlCache = cache as? SQLiteNormalizedCache else {
+            XCTFail("The cache is not using SQLite")
+            return
+          }
 
-    // Creates a new database, which will run all migrations
-    try SQLiteTestCacheProvider.withCache(fileURL: sqliteFileURL) { (cache) in
-      guard let sqlCache = cache as? SQLiteNormalizedCache else {
-        XCTFail("The cache is not using SQLite")
-        return
+          // verify that the current schema version is now incremented from the snapshot
+          let schemaVersion = try sqlCache.readSchemaVersion()
+          XCTAssertEqual(schemaVersion, Int64(previousSchemaVersion + 1))
+
+          // inserts some entries in the database to verify the file is useable after the migration
+          runTestFetchAndPersist(againstFileAt: fileURL)
+        }
       }
-      let version = try sqlCache.readSchemaVersion()
-      XCTAssertEqual(version, 1)
-    }
-
-    // Inserts some entries in the database
-    testFetchAndPersist()
   }
 
   func testFetchAndPersist() {
-    let query = HeroNameQuery()
-    let sqliteFileURL = SQLiteTestCacheProvider.temporarySQLiteFileURL()
-
-    SQLiteTestCacheProvider.withCache(fileURL: sqliteFileURL) { (cache) in
-      let store = ApolloStore(cache: cache)
-      let networkTransport = MockNetworkTransport(body: [
-        "data": [
-          "hero": [
-            "name": "Luke Skywalker",
-            "__typename": "Human"
-          ]
-        ]
-      ], store: store)
-      let client = ApolloClient(networkTransport: networkTransport, store: store)
-
-      let networkExpectation = self.expectation(description: "Fetching query from network")
-      let newCacheExpectation = self.expectation(description: "Fetch query from new cache")
-
-      client.fetch(query: query, cachePolicy: .fetchIgnoringCacheData) { outerResult in
-        defer { networkExpectation.fulfill() }
-        
-        switch outerResult {
-        case .failure(let error):
-          XCTFail("Unexpected error: \(error)")
-          return
-        case .success(let graphQLResult):
-          XCTAssertEqual(graphQLResult.data?.hero?.name, "Luke Skywalker")
-          // Do another fetch from cache to ensure that data is cached before creating new cache
-          client.fetch(query: query, cachePolicy: .returnCacheDataDontFetch) { innerResult in
-            SQLiteTestCacheProvider.withCache(fileURL: sqliteFileURL) { cache in
-              let newStore = ApolloStore(cache: cache)
-              let newClient = ApolloClient(networkTransport: networkTransport, store: newStore)
-              newClient.fetch(query: query, cachePolicy: .returnCacheDataDontFetch) { newClientResult in
-                defer { newCacheExpectation.fulfill() }
-                switch newClientResult {
-                case .success(let newClientGraphQLResult):
-                  XCTAssertEqual(newClientGraphQLResult.data?.hero?.name, "Luke Skywalker")
-                case .failure(let error):
-                  XCTFail("Unexpected error with new client: \(error)")
-                }
-                _ = newClient // Workaround for a bug - ensure that newClient is retained until this block is run
-              }
-            }
-          }
-        }
-      }
-      
-      self.waitForExpectations(timeout: 2, handler: nil)
-    }
+    self.runTestFetchAndPersist(againstFileAt: SQLiteTestCacheProvider.temporarySQLiteFileURL())
   }
 
   func testPassInConnectionDoesNotThrow() {
@@ -158,5 +112,62 @@ class CachePersistenceTests: XCTestCase {
 
       self.waitForExpectations(timeout: 2, handler: nil)
     }
+  }
+}
+
+extension CachePersistenceTests {
+  private func runTestFetchAndPersist(
+    againstFileAt sqliteFileURL: URL,
+    file: StaticString = #file,
+    line: UInt = #line
+  ) {
+      let query = HeroNameQuery()
+
+      SQLiteTestCacheProvider.withCache(fileURL: sqliteFileURL) { (cache) in
+        let store = ApolloStore(cache: cache)
+        let networkTransport = MockNetworkTransport(body: [
+          "data": [
+            "hero": [
+              "name": "Luke Skywalker",
+              "__typename": "Human"
+            ]
+          ]
+        ], store: store)
+        let client = ApolloClient(networkTransport: networkTransport, store: store)
+
+        let networkExpectation = self.expectation(description: "Fetching query from network")
+        let newCacheExpectation = self.expectation(description: "Fetch query from new cache")
+
+        client.fetch(query: query, cachePolicy: .fetchIgnoringCacheData) { outerResult in
+          defer { networkExpectation.fulfill() }
+
+          switch outerResult {
+          case .failure(let error):
+            XCTFail("Unexpected error: \(error)", file: file, line: line)
+            return
+          case .success(let graphQLResult):
+            XCTAssertEqual(graphQLResult.data?.hero?.name, "Luke Skywalker", file: file, line: line)
+            // Do another fetch from cache to ensure that data is cached before creating new cache
+            client.fetch(query: query, cachePolicy: .returnCacheDataDontFetch) { innerResult in
+              SQLiteTestCacheProvider.withCache(fileURL: sqliteFileURL) { cache in
+                let newStore = ApolloStore(cache: cache)
+                let newClient = ApolloClient(networkTransport: networkTransport, store: newStore)
+                newClient.fetch(query: query, cachePolicy: .returnCacheDataDontFetch) { newClientResult in
+                  defer { newCacheExpectation.fulfill() }
+                  switch newClientResult {
+                  case .success(let newClientGraphQLResult):
+                    XCTAssertEqual(newClientGraphQLResult.data?.hero?.name, "Luke Skywalker", file: file, line: line)
+                  case .failure(let error):
+                    XCTFail("Unexpected error with new client: \(error)", file: file, line: line)
+                  }
+                  _ = newClient // Workaround for a bug - ensure that newClient is retained until this block is run
+                }
+              }
+            }
+          }
+        }
+
+        self.waitForExpectations(timeout: 2, handler: nil)
+      }
   }
 }
