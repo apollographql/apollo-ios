0b4298809a17935b6671d5aaa94636164665b639
Martijn Walraven <martijn@martijnwalraven.com>
2020-11-24 08:09:34 +0100

    Make DataLoaderTests more readable

--- Tests/ApolloTests/DataLoaderTests.swift
+++ Tests/ApolloTests/DataLoaderTests.swift
@@ -5,78 +5,81 @@ import StarWarsAPI
 
 class DataLoaderTests: XCTestCase {
   func testSingleLoad() throws {
-    let loader = DataLoader<Int, Int> { keys in
-      return self.resultsForKeys(keys)
+    let loader = DataLoader<Int, String> { keys in
+      return self.wordsForNumbers(keys)
     }
     
-    XCTAssertEqual(try loader[1].get(), 1)
+    XCTAssertEqual(try loader[1].get(), "one")
   }
   
   func testMultipleLoads() throws {
     var numberOfBatchLoads = 0
     
-    let loader = DataLoader<Int, Int> { keys in
+    let loader = DataLoader<Int, String> { keys in
       numberOfBatchLoads += 1
-      return self.resultsForKeys(keys)
+      return self.wordsForNumbers(keys)
     }
         
-    let promises = [loader[1], loader[2]]
-    let values = try promises.map { try $0.get() }
+    let results = [loader[1], loader[2]]
+    let values = try results.map { try $0.get() }
     
-    XCTAssertEqual(values, [1, 2])
+    XCTAssertEqual(values, ["one", "two"])
     XCTAssertEqual(numberOfBatchLoads, 1)
   }
   
   func testCoalescesIdenticalRequests() throws {
     var batchLoads: [Set<Int>] = []
     
-    let loader = DataLoader<Int, Int> { keys in
+    let loader = DataLoader<Int, String> { keys in
       batchLoads.append(keys)
-      return self.resultsForKeys(keys)
+      return self.wordsForNumbers(keys)
     }
         
-    let promises = [loader[1], loader[1]]
-    let values = try promises.map { try $0.get() }
+    let results = [loader[1], loader[1]]
+    let values = try results.map { try $0.get() }
     
-    XCTAssertEqual(values, [1, 1])
+    XCTAssertEqual(values, ["one", "one"])
     XCTAssertEqual(batchLoads.count, 1)
     XCTAssertEqual(batchLoads[0], [1])
   }
   
   func testCachesRepeatedRequests() throws {
-    var batchLoads: [Set<String>] = []
+    var batchLoads: [Set<Int>] = []
     
-    let loader = DataLoader<String, String> { keys in
+    let loader = DataLoader<Int, String> { keys in
       batchLoads.append(keys)
-      return self.resultsForKeys(keys)
+      return self.wordsForNumbers(keys)
     }
         
-    let promises1 = [loader["A"], loader["B"]]
-    let values1 = try promises1.map { try $0.get() }
+    let results1 = [loader[1], loader[2]]
+    let values1 = try results1.map { try $0.get() }
     
-    XCTAssertEqual(values1, ["A", "B"])
+    XCTAssertEqual(values1, ["one", "two"])
     XCTAssertEqual(batchLoads.count, 1)
-    XCTAssertEqualUnordered(batchLoads[0], ["A", "B"])
+    XCTAssertEqualUnordered(batchLoads[0], [1, 2])
         
-    let promises2 = [loader["A"], loader["C"]]
-    let values2 = try promises2.map { try $0.get() }
+    let results2 = [loader[1], loader[3]]
+    let values2 = try results2.map { try $0.get() }
     
-    XCTAssertEqual(values2, ["A", "C"])
+    XCTAssertEqual(values2, ["one", "three"])
     XCTAssertEqual(batchLoads.count, 2)
-    XCTAssertEqual(batchLoads[1], ["C"])
+    XCTAssertEqual(batchLoads[1], [3])
     
-    let promises3 = [loader["A"], loader["B"], loader["C"]]
-    let values3 = try promises3.map { try $0.get() }
+    let results3 = [loader[1], loader[2], loader[3]]
+    let values3 = try results3.map { try $0.get() }
 
-    XCTAssertEqual(values3, ["A", "B", "C"])
+    XCTAssertEqual(values3, ["one", "two", "three"])
     XCTAssertEqual(batchLoads.count, 2)
   }
   
   // - Helpers
   
-  private func resultsForKeys<Key>(_ keys: Set<Key>) -> [Key: Key] {
+  private func wordsForNumbers(_ keys: Set<Int>) -> [Int: String] {
+    let formatter = NumberFormatter()
+    formatter.numberStyle = .spellOut
+    
     return keys.reduce(into: [:]) { result, key in
-      result[key] = key
+      result[key] = formatter.string(from: key as NSNumber)
     }
   }
 }
