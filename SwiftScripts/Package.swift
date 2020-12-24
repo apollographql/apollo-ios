8a66f206f2aef4c69352b0228f660e8703540687
Ellen Shapiro <designatednerd@gmail.com>
2020-12-23 20:40:44 -0600

    update version of sourceDocs

--- SwiftScripts/Package.swift
+++ SwiftScripts/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     dependencies: [
         .package(name: "Apollo", path: ".."),
         .package(url: "https://github.com/apple/swift-tools-support-core", from: "0.0.1"),
-        .package(url: "https://github.com/eneko/SourceDocs.git", .upToNextMinor(from: "1.1.0"))
+        .package(url: "https://github.com/eneko/SourceDocs.git", .upToNextMinor(from: "1.2.0"))
     ],
     targets: [
         .target(name: "Codegen",
