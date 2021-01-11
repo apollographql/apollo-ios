c0c7ea02c89dce78d5d805dd75ab636e33b4d3d5
Ellen Shapiro <designatednerd@gmail.com>
2021-01-11 11:34:28 -0600

    Update to version 2.32.1 of the typescript CLI

--- Sources/ApolloCodegenLib/CLIDownloader.swift
+++ Sources/ApolloCodegenLib/CLIDownloader.swift
@@ -30,7 +30,7 @@ struct CLIDownloader {
   }
   
   /// The URL string for getting the current version of the CLI
-  static let downloadURLString = "https://install.apollographql.com/legacy-cli/darwin/2.31.2"
+  static let downloadURLString = "https://install.apollographql.com/legacy-cli/darwin/2.32.1"
   
   /// Downloads the appropriate Apollo CLI in a zip file.
   ///
