642b83f6fcce6c7acf65a7ba3fdaabd54b35454d
Anthony Miller <AnthonyMDev@gmail.com>
2021-03-26 10:42:07 -0700

    WIP

---
+++ Sources/ApolloCodegenLib/V2/V2_CodeGenerator.swift
@@ -0,0 +1,11 @@
+//
+//  V2_CodeGenerator.swift
+//  ApolloCodegenLib
+//
+//  Created by Anthony Miller on 3/23/21.
+//  Copyright © 2021 Apollo GraphQL. All rights reserved.
+//
+
+class V2_CodeGenerator {
+
+}
