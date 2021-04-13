18bf7fecb3911059d75f06557571887653f396bb
Gwendal Roué <gr@pierlis.com>
2021-04-13 13:51:20 +0200

    Atomic.mutate(block:) can return a value

--- Sources/ApolloCore/Atomic.swift
+++ Sources/ApolloCore/Atomic.swift
@@ -22,10 +22,11 @@ public class Atomic<T> {
   
   /// Mutates the underlying value within a lock.
   /// - Parameter block: The block to execute to mutate the value.
-  public func mutate(block: (inout T) -> Void) {
+  public func mutate<U>(block: (inout T) -> U) -> U {
     lock.lock()
-    block(&_value)
+    let result = block(&_value)
     lock.unlock()
+    return result
   }
 }
 
