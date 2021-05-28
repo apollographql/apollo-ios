2e546a527096e036da759a2fa1e3e18132444995
Ellen Shapiro <designatednerd@gmail.com>
2021-05-28 14:57:55 -0500

    Revert "Add Apollo-Use-Only method to create a non-type safe GraphQL request"
    This reverts commit 9aac6367a611260ffea12e3b0ae965b999d9b9d3.

--- Sources/Apollo/RequestBodyCreator.swift
+++ Sources/Apollo/RequestBodyCreator.swift
@@ -21,48 +21,18 @@ public protocol RequestBodyCreator {
 // MARK: - Default Implementation
 
 extension RequestBodyCreator {
-
+  
   public func requestBody<Operation: GraphQLOperation>(for operation: Operation,
                                                        sendOperationIdentifiers: Bool,
                                                        sendQueryDocument: Bool,
                                                        autoPersistQuery: Bool) -> GraphQLMap {
-    
-    return self.__requestBody(for: operation.queryDocument,
-                            variables: operation.variables,
-                            operationName: operation.operationName,
-                            sendQueryDocument: sendQueryDocument,
-                            sendOperationIdentifiers: sendOperationIdentifiers,
-                            operationIdentifier: operation.operationIdentifier,
-                            autoPersistQuery: autoPersistQuery)
-  }
-  
-  /// A non-type-safe request creator to facilitate sending requests not using code generation.
-  ///
-  /// Note: This is only public for use in `ApolloCodegenLib`, we do **not** recommend or support direct use of this in client applications.
-  /// - Parameters:
-  ///   - operationDocument: The query/mutation/subscription document, as a string
-  ///   - variables: [optional] Any variables to send with the operation
-  ///   - operationName: The name of the operation being sent
-  ///   - sendQueryDocument: If the query document should be sent - defaults to true.
-  ///   - sendOperationIdentifiers: If operation identifers should be sent. Defaults to false
-  ///   - operationIdentifier: [Optional] The operation identifier to use, defaults to nil
-  ///   - autoPersistQuery: Whether the query should be auto-persisted, defaults to false.
-  /// - Returns: The body for the given request, ready to be added as the `httpBody`.
-  public func __requestBody(for operationDocument: String,
-                            variables: GraphQLMap?,
-                            operationName: String,
-                            sendQueryDocument: Bool = true,
-                            sendOperationIdentifiers: Bool = false,
-                            operationIdentifier: String? = nil,
-                            autoPersistQuery: Bool = false) -> GraphQLMap {
-    
     var body: GraphQLMap = [
-      "variables": variables,
-      "operationName": operationName,
+      "variables": operation.variables,
+      "operationName": operation.operationName,
     ]
 
     if sendOperationIdentifiers {
-      guard let operationIdentifier = operationIdentifier else {
+      guard let operationIdentifier = operation.operationIdentifier else {
         preconditionFailure("To send operation identifiers, Apollo types must be generated with operationIdentifiers")
       }
 
@@ -70,11 +40,11 @@ extension RequestBodyCreator {
     }
 
     if sendQueryDocument {
-      body["query"] = operationDocument
+      body["query"] = operation.queryDocument
     }
 
     if autoPersistQuery {
-      guard let operationIdentifier = operationIdentifier else {
+      guard let operationIdentifier = operation.operationIdentifier else {
         preconditionFailure("To enable `autoPersistQueries`, Apollo types must be generated with operationIdentifiers")
       }
 
