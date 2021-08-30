b38a08d0ad414878e1d1290f7e4bb1899c308634
Calvin Cestari <calvin.cestari@apollographql.com>
2021-08-30 14:16:22 -0700

    Add missing method header documentation

--- Sources/ApolloWebSocket/OperationMessageIdCreator.swift
+++ Sources/ApolloWebSocket/OperationMessageIdCreator.swift
@@ -9,14 +9,18 @@ public protocol OperationMessageIdCreator {
 
 // MARK: - Default Implementation
 
+/// The default implementation of `OperationMessageIdCreator` that uses a sequential numbering scheme.
 public struct ApolloSequencedOperationMessageIdCreator: OperationMessageIdCreator {
   private var sequenceNumberCounter = Atomic<Int>(0)
-  
-  // Internal init methods cannot be used in public methods
+
+  /// Designated initializer.
+  ///
+  /// - Parameter startAt: The number from which the sequenced numbering scheme should start.
   public init(startAt sequenceNumber: Int = 1) {
     sequenceNumberCounter = Atomic<Int>(sequenceNumber)
   }
-  
+
+  /// Returns the number in the current sequence. Will be incremented when calling this method.
   public func requestId() -> String {
     let id = sequenceNumberCounter.value
     _ = sequenceNumberCounter.increment()
