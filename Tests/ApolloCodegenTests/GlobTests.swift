704bc6f8bf590ff7b02b7a6d6895aa2ea9f2d635
Calvin Cestari <calvin.cestari@apollographql.com>
2021-11-22 15:40:57 -0800

    Add tests for basic glob patterns

--- Tests/ApolloCodegenTests/GlobTests.swift
+++ Tests/ApolloCodegenTests/GlobTests.swift
@@ -1,7 +1,147 @@
 import XCTest
 import Nimble
-import ApolloCodegenLib
+@testable import ApolloCodegenLib
+import ApolloCodegenTestSupport
 
 class GlobTests: XCTestCase {
-  
+  let baseURL = CodegenTestHelper.outputFolderURL().appendingPathComponent("Glob")
+
+  // MARK: Setup
+
+  override func setUpWithError() throws {
+    let fileManager = FileManager.default.apollo
+
+    // <outputFolder>/Glob/
+    try fileManager.createDirectoryIfNeeded(atPath: baseURL.path)
+
+    expect(
+      // <outputFolder>/Glob/file.one
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("file.one").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/file.two
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("file.two").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/a/file.one
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("a/file.one").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/a/b/file.one
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("a/b/file.one").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/a/b/c/file.one
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("a/b/c/file.one").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/a/b/c/d/e/f/file.one
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("a/b/c/d/e/f/file.one").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/other/file.one
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("other/file.one").path)
+    ).to(beTrue())
+
+    expect(
+      // <outputFolder>/Glob/other/file.oye
+      try fileManager.createFile(atPath: self.baseURL.appendingPathComponent("other/file.oye").path)
+    ).to(beTrue())
+  }
+
+  override func tearDownWithError() throws {
+    try FileManager.default.apollo.deleteDirectory(atPath: baseURL.path)
+  }
+
+  // MARK: Tests
+
+  func test_paths_givenNoMatch_whenMultipleFiles_shouldReturnEmpty() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("*.xyz").path
+
+    // when
+    // <outputFolder>/Glob/file.one
+    // <outputFolder>/Glob/file.two
+
+    // then
+    expect(Glob(pattern).paths).to(beEmpty())
+  }
+
+  func test_paths_givenSingleMatch_whenMultipleFiles_shouldReturnSingle() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("*.one").path
+
+    // when
+    // <outputFolder>/Glob/file.one
+    // <outputFolder>/Glob/file.two
+
+    // then
+    expect(Glob(pattern).paths).to(haveCount(1))
+  }
+
+  func test_paths_givenMultipleMatches_whenMultipleFiles_shouldReturnMultiple() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("file.*").path
+
+    // when
+    // <outputFolder>/Glob/file.one
+    // <outputFolder>/Glob/file.two
+
+    // then
+    expect(Glob(pattern).paths).to(haveCount(2))
+  }
+
+  func test_paths_givenMultipleMatchBraces_whenSingleFile_shouldReturnSingle() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("a/{*.one,*.two}").path
+
+    // when
+    // <outputFolder>/Glob/a/file.one
+
+    expect(Glob(pattern).paths).to(haveCount(1))
+  }
+
+  func test_paths_givenMultipleMatchBraces_whenMultipleFiles_shouldReturnMultiple() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("{*.one,*.two}").path
+
+    // when
+    // <outputFolder>/Glob/file.one
+    // <outputFolder>/Glob/file.two
+
+    // then
+    expect(Glob(pattern).paths).to(haveCount(2))
+  }
+
+  func test_paths_givenMultipleMatchBraces_withNegation_whenMultipleFiles_shouldReturnSingle() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("{*.one,!*.two}").path
+
+    // when
+    // <outputFolder>/Glob/file.one
+    // <outputFolder>/Glob/file.two
+
+    // then
+    expect(Glob(pattern).paths).to(haveCount(1))
+  }
+
+  func test_paths_givenMultipleMatches_withWildcardCharacter_whenMultipleFiles_shouldReturnMultiple() throws {
+    // given
+    let pattern = baseURL.appendingPathComponent("other/file.o?e").path
+
+    // when
+    // <outputFolder>/Glob/other/file.one
+    // <outputFolder>/Glob/other/file.oye
+
+    // then
+    expect(Glob(pattern).paths).to(haveCount(2))
+  }
+
+  #warning("TODO - memory leak test")
 }
