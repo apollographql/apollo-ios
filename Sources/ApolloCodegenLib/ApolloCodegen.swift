7e53066485f0ebeb17173c891f2f71c243b00ffe
Calvin Cestari <calvin.cestari@apollographql.com>
2021-12-08 12:37:54 -0800

    Call GraphQL frontend to compile results from input configuration

--- Sources/ApolloCodegenLib/ApolloCodegen.swift
+++ Sources/ApolloCodegenLib/ApolloCodegen.swift
@@ -5,12 +5,47 @@ import Foundation
 
 /// A class to facilitate running code generation
 public class ApolloCodegen {
+  public enum Error: Swift.Error, LocalizedError {
+    case validationFailed(atLines: [String])
+
+    public var errorDescription: String? {
+      switch self {
+      case let .validationFailed(lines):
+        return "Schema and Operation validation failed! Check \(lines)"
+      }
+    }
+  }
   /// Executes the code generation engine with a specified configuration.
   ///
   /// - Parameters:
   ///   - configuration: The configuration to use to build the code generation.
   public static func build(with configuration: ApolloCodegenConfiguration) throws {
     try configuration.validate()
+
+    let compilationResult = try compileResults(using: configuration.input)
+    #warning("TODO - compilationResult will be passed into the next step")
+  }
+
+  static func compileResults(
+    using input: ApolloCodegenConfiguration.FileInput
+  ) throws -> CompilationResult {
+    let frontend = try ApolloCodegenFrontend()
+
+    let schemaURL = URL(fileURLWithPath: input.schemaPath)
+    let graphqlSchema = try frontend.loadSchema(from: schemaURL)
+
+    let matches = try Glob(input.searchPaths).match()
+    let documents = try matches.map({ path in
+      return try frontend.parseDocument(from: URL(fileURLWithPath: path))
+    })
+    let mergedDocument = try frontend.mergeDocuments(documents)
+
+    let graphqlErrors = try frontend.validateDocument(schema: graphqlSchema, document: mergedDocument)
+    guard graphqlErrors.isEmpty else {
+      throw Error.validationFailed(atLines: graphqlErrors.flatMap({ $0.logLines }))
+    }
+
+    return try frontend.compile(schema: graphqlSchema, document: mergedDocument)
   }
 }
 
---
+++ Tests/ApolloCodegenTests/ApolloCodegenTests.swift
@@ -0,0 +1,193 @@
+import XCTest
+import ApolloCodegenTestSupport
+@testable import ApolloCodegenLib
+import Nimble
+
+class ApolloCodegenTests: XCTestCase {
+  private let schemaSDL: String = {
+    """
+    type Query {
+      books: [Book!]!
+      authors: [Author!]!
+    }
+
+    type Book {
+      title: String!
+      author: Author!
+    }
+
+    type Author {
+      name: String!
+      books: [Book!]!
+    }
+    """
+  }()
+
+  let directoryURL = CodegenTestHelper.outputFolderURL().appendingPathComponent("Codegen")
+
+  override func setUpWithError() throws {
+    try FileManager.default.apollo.createDirectoryIfNeeded(atPath: directoryURL.path)
+  }
+
+  override func tearDownWithError() throws {
+    try cleanTestOutput()
+  }
+
+  // MARK: Helpers
+
+  private func cleanTestOutput() throws {
+    try FileManager.default.apollo.deleteDirectory(atPath: directoryURL.path)
+  }
+
+  private func createSchema(at path: String) {
+    expect(
+      try FileManager.default.apollo.createFile(
+        atPath: path,
+        data: self.schemaSDL.data(using: .utf8)!
+      )
+    ).notTo(throwError())
+  }
+
+  private func createOperationFile(for data: Data, inDirectory directoryURL: URL) {
+    expect(
+      try FileManager.default.apollo.createFile(
+        atPath: directoryURL.appendingPathComponent("\(UUID().uuidString).graphql").path,
+        data: data
+      )
+    ).notTo(throwError())
+  }
+
+  // MARK: Tests
+
+  func test_build_givenInvalidConfiguration_shouldThrow() throws {
+    // given
+    let config = ApolloCodegenConfiguration(basePath: "not_a_path")
+
+    // then
+    expect(try ApolloCodegen.build(with: config)).to(throwError())
+  }
+
+  /// ```
+  /// SCHEMA:
+  /// type Query {
+  ///   books: [Book!]!
+  ///   authors: [Author!]!
+  /// }
+  ///
+  /// type Book {
+  ///   title: String!
+  ///   author: Author!
+  /// }
+  ///
+  /// type Author {
+  ///   name: String!
+  ///   books: [Book!]!
+  /// }
+  ///
+  /// OPERATION:
+  /// query getBooks {
+  ///   books {
+  ///     title
+  ///     name (this will cause the validation error)
+  ///   }
+  /// }
+  /// ```
+  func test_compileResults_givenOperation_withGraphQLErrors_shouldThrow() throws {
+    // given
+    let schemaPath = directoryURL.appendingPathComponent("schema.graphqls").path
+    createSchema(at: schemaPath)
+
+    let searchPath = directoryURL.appendingPathComponent("*.graphql").path
+    let data: Data =
+      """
+      query getBooks {
+        books {
+          title
+          name
+        }
+      }
+      """.data(using: .utf8)!
+    createOperationFile(for: data, inDirectory: directoryURL)
+
+    let config = ApolloCodegenConfiguration.FileInput(
+      schemaPath: schemaPath,
+      searchPaths: [searchPath]
+    )
+
+    // then
+    expect(try ApolloCodegen.compileResults(using: config))
+    .to(throwError { error in
+      guard case let ApolloCodegen.Error.validationFailed(lines) = error else {
+        fail("Expected .validationFailed, got .\(error)")
+        return
+      }
+      expect(lines).notTo(beEmpty())
+    })
+  }
+
+  /// ```
+  /// SCHEMA:
+  /// type Query {
+  ///   books: [Book!]!
+  ///   authors: [Author!]!
+  /// }
+  ///
+  /// type Book {
+  ///   title: String!
+  ///   author: Author!
+  /// }
+  ///
+  /// type Author {
+  ///   name: String!
+  ///   books: [Book!]!
+  /// }
+  ///
+  /// OPERATION 1:
+  /// query getBooks {
+  ///   books {
+  ///     title
+  ///   }
+  /// }
+  ///
+  /// OPERATION 2:
+  /// query getAuthors {
+  ///   authors {
+  ///     name
+  ///   }
+  /// }
+  /// ```
+  func test_compileResults_givenOperations_withNoErrors_shouldReturn() throws {
+    // given
+    let schemaPath = directoryURL.appendingPathComponent("schema.graphqls").path
+    createSchema(at: schemaPath)
+
+    let searchPath = directoryURL.appendingPathComponent("*.graphql").path
+    let booksData: Data =
+      """
+      query getBooks {
+        books {
+          title
+        }
+      }
+      """.data(using: .utf8)!
+    createOperationFile(for: booksData, inDirectory: directoryURL)
+
+    let authorsData: Data =
+      """
+      query getAuthors {
+        authors {
+          name
+        }
+      }
+      """.data(using: .utf8)!
+    createOperationFile(for: authorsData, inDirectory: directoryURL)
+
+    let config = ApolloCodegenConfiguration.FileInput(
+      schemaPath: schemaPath,
+      searchPaths: [searchPath]
+    )
+
+    // then
+    expect(try ApolloCodegen.compileResults(using: config).operations).to(haveCount(2))
+  }
+}
