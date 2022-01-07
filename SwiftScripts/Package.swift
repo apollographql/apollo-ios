94a650f439c494b8ea7b341935001bc8d4d9e855
Calvin Cestari <calvin.cestari@apollographql.com>
2022-01-07 15:00:32 -0800

    Update SourceDocsLib dependecy

--- SwiftScripts/Package.swift
+++ SwiftScripts/Package.swift
@@ -6,12 +6,12 @@ import PackageDescription
 let package = Package(
     name: "Codegen",
     platforms: [
-        .macOS(.v10_14)
+        .macOS(.v10_15)
     ],
     dependencies: [
         .package(name: "Apollo", path: ".."),
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "0.3.0"),
-        .package(url: "https://github.com/eneko/SourceDocs.git", .upToNextMinor(from: "1.2.0"))
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.0.2")),
+        .package(url: "https://github.com/eneko/SourceDocs.git", .upToNextMinor(from: "2.0.0"))
     ],
     targets: [
         .target(name: "Codegen",
