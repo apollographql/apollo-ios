41c6f6817da22c7eafc038e72e1234c4b8f4a48b
Calvin Cestari <calvin.cestari@apollographql.com>
2022-01-25 17:57:18 -0800

    Use UnionTemplate in UnionFileGenerator

--- Sources/ApolloCodegenLib/FileGenerators/UnionFileGenerator.swift
+++ Sources/ApolloCodegenLib/FileGenerators/UnionFileGenerator.swift
@@ -4,20 +4,25 @@ import Foundation
 struct UnionFileGenerator: FileGenerator, Equatable {
   /// The `GraphQLUnionType` object used to build the file content.
   let unionType: GraphQLUnionType
+  /// The name of the generated Swift code module.
+  let moduleName: String
   let path: String
 
   var data: Data? {
-    #warning("TODO: need correct data template")
-    return "public enum \(unionType.name) {}".data(using: .utf8)
+    UnionTemplate(moduleName: moduleName, graphqlUnion: unionType)
+      .render()
+      .data(using: .utf8)
   }
 
   /// Designated initializer.
   ///
   /// - Parameters:
   ///  - unionType: The `GraphQLUnionType` object used to build the file content.
+  ///  - moduleName: The name of the generated Swift code module.
   ///  - directoryPath: The **directory** path that the file should be written to, used to build the `path` property value.
-  init(unionType: GraphQLUnionType, directoryPath: String) {
+  init(unionType: GraphQLUnionType, moduleName: String, directoryPath: String) {
     self.unionType = unionType
+    self.moduleName = moduleName
 
     self.path = URL(fileURLWithPath: directoryPath)
       .appendingPathComponent("\(unionType.name).swift").path
