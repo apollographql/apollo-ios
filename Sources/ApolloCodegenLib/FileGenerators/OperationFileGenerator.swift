b2d3db9e4a2e8c2904b509af73626f9dcee1a5ea
Calvin Cestari <calvin.cestari@apollographql.com>
2022-01-31 21:38:09 -0800

    Add tests for FragmentFileGenerator

--- Sources/ApolloCodegenLib/FileGenerators/OperationFileGenerator.swift
+++ Sources/ApolloCodegenLib/FileGenerators/OperationFileGenerator.swift
@@ -5,10 +5,10 @@ struct OperationFileGenerator {
   /// Converts `graphQLOperation` into Swift code and writes the result to a file in `directoryPath`.
   ///
   /// - Parameters:
-  ///   - graphQLOperation: `IR.Operation` object used to build the file content.
-  ///   - schema: `IR.Schema` on which the operation executes.
+  ///   - graphQLOperation: The `IR.Operation` object used to build the file content.
+  ///   - schema: The `IR.Schema` the fragment is belongs to.
   ///   - config: `ApolloCodegenConfiguration` object that defines behavior for the Swift code generation.
-  ///   - directoryPath: Output path that the file will be written to.
+  ///   - directoryPath: The output path that the file will be written to.
   ///   - fileManager: `FileManager` object used to create the file. Defaults to `FileManager.default`.
   static func generate(
     _ graphQLOperation: IR.Operation,
---
+++ Tests/ApolloCodegenTests/CodeGeneration/FileGenerators/FragmentFileGeneratorTests.swift
@@ -0,0 +1,62 @@
+import XCTest
+import Nimble
+@testable import ApolloCodegenLib
+import ApolloCodegenTestSupport
+
+class FragmentFileGeneratorTests: XCTestCase {
+  override func tearDown() {
+    CodegenTestHelper.deleteExistingOutputFolder()
+
+    super.tearDown()
+  }
+
+  func test_generate_givenSchemaType_shouldWriteToCorrectFilePath() throws {
+    // given
+    let schemaSDL = """
+    type Animal {
+      species: String
+    }
+
+    type Query {
+      animals: [Animal]
+    }
+    """
+
+    let operationDocument = """
+    query AllAnimals {
+      animals {
+        ...AnimalDetails
+      }
+    }
+
+    fragment AnimalDetails on Animal {
+      species
+    }
+    """
+
+    let ir = try IR.mock(schema: schemaSDL, document: operationDocument)
+    let irFragment = ir.build(fragment: ir.compilationResult.fragments[0])
+    let config = ApolloCodegenConfiguration.mock()
+
+    let fileManager = MockFileManager(strict: false)
+    let rootURL = URL(fileURLWithPath: CodegenTestHelper.outputFolderURL().path)
+    let fileURL = rootURL.appendingPathComponent("AnimalDetails.swift")
+
+    fileManager.mock(closure: .createFile({ path, data, attributes in
+      expect(path).to(equal(fileURL.path))
+
+      return true
+    }))
+
+    // then
+    try FragmentFileGenerator.generate(
+      irFragment,
+      schema: ir.schema,
+      config: config,
+      directoryPath: rootURL.path,
+      fileManager: fileManager
+    )
+
+    expect(fileManager.allClosuresCalled).to(beTrue())
+  }
+}
