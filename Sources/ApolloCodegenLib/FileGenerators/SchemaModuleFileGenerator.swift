4ded787f6f672eeb7b12c826127aa3127fad91ce
Calvin Cestari <calvin.cestari@apollographql.com>
2022-02-01 23:20:17 -0800

    Refactor SchemaModuleFileGenerator for more functional behavior

--- Sources/ApolloCodegenLib/FileGenerators/SchemaModuleFileGenerator.swift
+++ Sources/ApolloCodegenLib/FileGenerators/SchemaModuleFileGenerator.swift
@@ -1,63 +1,48 @@
 import Foundation
 
-/// Generates a package manifest file for the releveant depdency manager.
-struct SchemaModuleFileGenerator: FileGenerator, Equatable {
-  var path: String { fileGenerator.path }
-  var data: Data? { fileGenerator.data }
-
-  private let fileGenerator: FileGenerator
-
-  /// Designated initializer.
+struct SchemaModuleFileGenerator {
+  /// Generates a package manifest file for the releveant depdency manager.
   ///
   /// - Parameters:
-  ///  - configuration: A configuration object that specifies properties such as which dependency manager and where the file
-  ///  should be output to.
-  init(_ configuration: ApolloCodegenConfiguration.SchemaTypesFileOutput) throws {
-    switch configuration.dependencyAutomation {
+  ///   - config: A configuration object specifying output behavior.
+  ///   - fileManager: `FileManager` object used to create the file. Defaults to `FileManager.default`.
+  static func generate(
+    _ config: ApolloCodegenConfiguration.SchemaTypesFileOutput,
+    fileManager: FileManager = FileManager.default
+  ) throws {
+    switch config.dependencyAutomation {
     case let .swiftPackageManager(moduleName):
-      self.fileGenerator = SwiftPackageManagerFileGenerator(
-        moduleName: moduleName,
-        directoryPath: configuration.path
+      try SwiftPackageManagerFileGenerator.generate(
+        moduleName,
+        directoryPath: config.path,
+        fileManager: fileManager
       )
 
     default:
       throw NSError(domain: "ApolloCodegen", code: -1, userInfo: [NSLocalizedDescriptionKey: "Only Swift Package Manager is supported at the moment!"])
     }
   }
-
-  func generateFile(fileManager: FileManager = FileManager.default) throws {
-    try fileGenerator.generateFile(fileManager: fileManager)
-  }
-
-  static func == (lhs: SchemaModuleFileGenerator, rhs: SchemaModuleFileGenerator) -> Bool {
-    switch (lhs.fileGenerator, rhs.fileGenerator) {
-    case let (lhs as SwiftPackageManagerFileGenerator, rhs as SwiftPackageManagerFileGenerator):
-      return lhs == rhs
-    default:
-      return false
-    }
-  }
 }
 
-/// Generates a Package.swift file for Swift Package Manager support of the generated schema module files.
-fileprivate struct SwiftPackageManagerFileGenerator: FileGenerator, Equatable {
-  let path: String
-  /// The name of the library and target of the module.
-  let moduleName: String
-
-  var data: Data? {
-    SwiftPackageManagerModuleTemplate(moduleName: moduleName)
-      .render()
-      .data(using: .utf8)
-  }
-
-  /// Designated initializer.
+fileprivate struct SwiftPackageManagerFileGenerator {
+  /// Generates a Package.swift file for Swift Package Manager support of the generated schema module files.
   ///
   /// - Parameters:
   ///   - moduleName: The name of the library and target of the module.
-  ///   - directoryPath: The directory path where the package manifest will be output.
-  init(moduleName: String, directoryPath: String) {
-    self.moduleName = moduleName
-    self.path = URL(fileURLWithPath: directoryPath).appendingPathComponent("Package.swift").path
+  ///   - directoryPath: The **directory** path that the file should be written to, used to build the `path` property value.
+  ///   - fileManager: `FileManager` object used to create the file.
+  static func generate(
+    _ moduleName: String,
+    directoryPath: String,
+    fileManager: FileManager
+  ) throws {
+    let filePath = URL(fileURLWithPath: directoryPath)
+      .appendingPathComponent("Package.swift").path
+
+    let data = SwiftPackageManagerModuleTemplate(
+      moduleName: moduleName
+    ).render().data(using: .utf8)
+
+    try fileManager.apollo.createFile(atPath: filePath, data: data)
   }
 }
