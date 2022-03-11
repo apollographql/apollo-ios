bb2faf4d0a8a40323f65e7a07dccf82b387e9503
Calvin Cestari <calvin.cestari@apollographql.com>
2022-03-11 15:21:05 -0800

    Refactor SchemaModuleFileGenerator to use the new template and simplify logic

--- Sources/ApolloCodegenLib/FileGenerators/SchemaModuleFileGenerator.swift
+++ Sources/ApolloCodegenLib/FileGenerators/SchemaModuleFileGenerator.swift
@@ -10,43 +10,24 @@ struct SchemaModuleFileGenerator {
     _ config: ApolloCodegenConfiguration.SchemaTypesFileOutput,
     fileManager: FileManager = FileManager.default
   ) throws {
+
+    let path: String
+    let rendered: String
+
     switch config.moduleType {
     case .swiftPackageManager:
-      try SwiftPackageManagerFileGenerator.generate(
-        config.schemaName,
-        directoryPath: config.path,
-        fileManager: fileManager
-      )
+      path = URL(fileURLWithPath: config.path).appendingPathComponent("Package.swift").path
+      rendered = SwiftPackageManagerModuleTemplate(moduleName: config.schemaName).render()
+
+    case .none:
+      path = URL(fileURLWithPath: config.path).appendingPathComponent("\(config.schemaName).swift").path
+      rendered = SchemaModuleNamespaceTemplate(moduleName: config.schemaName).render()
 
     case .other:
       // no-op - the implementation is import statements in the generated operation files
       return
-
-    default:
-      throw NSError(domain: "ApolloCodegen", code: -1, userInfo: [NSLocalizedDescriptionKey: "Only Swift Package Manager is supported at the moment!"])
     }
-  }
-}
 
-fileprivate struct SwiftPackageManagerFileGenerator {
-  /// Generates a Package.swift file for Swift Package Manager support of the generated schema module files.
-  ///
-  /// - Parameters:
-  ///   - moduleName: The name of the library and target of the module.
-  ///   - directoryPath: The **directory** path that the file should be written to, used to build the `path` property value.
-  ///   - fileManager: `FileManager` object used to create the file.
-  static func generate(
-    _ moduleName: String,
-    directoryPath: String,
-    fileManager: FileManager
-  ) throws {
-    let filePath = URL(fileURLWithPath: directoryPath)
-      .appendingPathComponent("Package.swift").path
-
-    let data = SwiftPackageManagerModuleTemplate(
-      moduleName: moduleName
-    ).render().data(using: .utf8)
-
-    try fileManager.apollo.createFile(atPath: filePath, data: data)
+    try fileManager.apollo.createFile(atPath: path, data: rendered.data(using: .utf8))
   }
 }
