8f5d5ca4484eccc533f7c5a8ca11fa7c92cdbb0a
Calvin Cestari <calvin.cestari@apollographql.com>
2022-03-25 14:41:52 -0700

    Add TemplateRenderer tests

--- Sources/ApolloUtils/ReferenceWrapped.swift
+++ Sources/ApolloUtils/ReferenceWrapped.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 /// Used to wrap a value type to prevent copy on argument passing.
+@dynamicMemberLookup
 public class ReferenceWrapped<T> {
   /// Used to access the underlying wrapped value type.
   public let value: T
@@ -10,5 +11,9 @@ public class ReferenceWrapped<T> {
   /// - Parameter value: The value type that will be wrapped and available through the `value` property.
   public init (value: T) {
     self.value = value
+  }
+
+  public subscript<V>(dynamicMember keyPath: KeyPath<T, V>) -> V {
+    value[keyPath: keyPath]
   }
 }
