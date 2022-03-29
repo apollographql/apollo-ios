eb4ff26764a4434318d4a0608bc43a59051ea764
Calvin Cestari <calvincestari@users.noreply.github.com>
2022-03-29 11:01:09 -0700

    Update Module to be based off ModuleType enum (#2227)
    This is better than duplicating the enum values, now this simply needs to parse the input.

--- SwiftScripts/Sources/Codegen/Module.swift
+++ SwiftScripts/Sources/Codegen/Module.swift
@@ -1,37 +1,29 @@
 import Foundation
 import ApolloCodegenLib
 
-enum Module {
-  case swiftPackageManager(name: String)
-  case cocoapods(name: String)
-  case carthage(name: String)
-  case manuallyLinked(name: String)
+struct Module {
+  private let module: ApolloCodegenConfiguration.SchemaTypesFileOutput.ModuleType
 
-  init?(module: String, name: String) {
-    switch module {
-    case "SwiftPackageManager", "SPM": self = .swiftPackageManager(name: name)
-    case "CocoaPods": self = .cocoapods(name: name)
-    case "Carthage": self = .carthage(name: name)
-    case "ManuallyLinked": self = .manuallyLinked(name: name)
+  init?(module: String) {
+    switch module.lowercased() {
+    case "none": self.module = .none
+    case "swiftpackagemanager", "spm": self.module = .swiftPackageManager
+    case "other": self.module = .other
     default: return nil
     }
   }
 
-  func outputConfig(toTargetRoot targetRootURL: Foundation.URL) -> ApolloCodegenConfiguration.FileOutput {
+  func outputConfig(
+    toTargetRoot targetRootURL: Foundation.URL,
+    schemaName: String
+  ) -> ApolloCodegenConfiguration.FileOutput {
     let targetPath = targetRootURL.appendingPathComponent("Generated").path
-
-    let moduleType: ApolloCodegenConfiguration.SchemaTypesFileOutput.ModuleType
-    switch self {
-    case let .swiftPackageManager(name): moduleType = .swiftPackageManager(moduleName: name)
-    case let .cocoapods(name): moduleType = .cocoaPods(moduleName: name)
-    case let .carthage(name): moduleType = .carthage(moduleName: name)
-    case let .manuallyLinked(name): moduleType = .manuallyLinked(namespace: name)
-    }
 
     return ApolloCodegenConfiguration.FileOutput(
       schemaTypes: .init(
         path: targetPath,
-        dependencyAutomation: moduleType
+        schemaName: schemaName,
+        moduleType: module
       ),
       operations: .inSchemaModule,
       operationIdentifiersPath: nil
