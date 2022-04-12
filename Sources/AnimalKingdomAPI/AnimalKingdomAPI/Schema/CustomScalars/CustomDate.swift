2a5d1dd2390cc3dbfb49d1ce04349eeb98ebd746
Calvin Cestari <calvin.cestari@apollographql.com>
2022-04-12 11:19:32 -0700

    chore: Update CustomDate.swift with new header comment

--- Sources/AnimalKingdomAPI/AnimalKingdomAPI/Schema/CustomScalars/CustomDate.swift
+++ Sources/AnimalKingdomAPI/AnimalKingdomAPI/Schema/CustomScalars/CustomDate.swift
@@ -1,5 +1,9 @@
 // @generated
-// This file was automatically generated and should not be edited.
+// This file was automatically generated and can be edited to implement
+// advanced custom scalar functionality.
+//
+// Any changes to this file will not be overwritten by future
+// code generation execution.
 
 import ApolloAPI
 
