ef4866a75cc4803777adcc60b91648ee6893f0b8
Calvin Cestari <calvincestari@users.noreply.github.com>
2022-06-17 11:46:44 -0700

    feat: Prepare apollo-ios-cli for Homebrew formula submission (#2319)
    * refactor: Update CLI version to match the next alpha release
    * refactor: Update Apollo dependency to use branch instead of relative path

--- CodegenCLI/Sources/main.swift
+++ CodegenCLI/Sources/main.swift
@@ -5,7 +5,7 @@ struct CodegenCLI: ParsableCommand {
   static var configuration = CommandConfiguration(
     commandName: "apollo-ios-cli",
     abstract: "A command line utility for Apollo iOS code generation.",
-    version: "1.0.0",
+    version: "1.0.0-alpha.8",
     subcommands: [
       Initialize.self,
       Validate.self,
