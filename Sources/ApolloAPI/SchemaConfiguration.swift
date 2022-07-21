b47992181a35ed95c9daa1b15bbd38296eb34711
Anthony Miller <anthonymdev@gmail.com>
2022-07-21 11:06:20 -0700

    Add Documentation for CacheReference

--- Sources/ApolloAPI/SchemaConfiguration.swift
+++ Sources/ApolloAPI/SchemaConfiguration.swift
@@ -30,6 +30,24 @@ extension SchemaConfiguration {
   }
 }
 
+#warning("TODO docs")
+/// The key for an object must be:
+///   - Unique across the type
+///     - No two different objects with the same "__typename" can have the same key.
+///     - Keys do not need to be unique from keys for different types (objects with
+///      different "__typename"s).
+///   - Stable
+///     - The key for an object may not ever change. If the cache recieves a new key, it will
+///     treat the object as an entirely new object. There is no mechanisim for cache normalization
+///     in which an object changes its key but maintains its identity.
+///
+/// Any format for keys will work, as long as they are stable and unique.
+/// If multiple fields must be used to derive a unique key, we recommend joining the values for
+/// the fields with a ":" delimiter. For example, if you need to join the title of a book and the
+/// author name to use as a unique key, you could return "Iliad:Homer".
+///
+/// A reference to a record that does not have it's own unique cache key is based on a path from
+/// another cache reference or a root object.
 public protocol CacheKeyProvider {
   static var uniqueKeyGroupId: StaticString? { get }
   static func cacheKey(for data: JSONObject) -> String?
