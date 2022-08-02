5f31e2aa7239da315a0ca607320771dc99644347
Calvin Cestari <calvincestari@users.noreply.github.com>
2022-08-02 12:47:23 +0200

    fix: checkout at tag with legacy optionals behavior (#2412)

--- scripts/install-or-update-starwars-server.sh
+++ scripts/install-or-update-starwars-server.sh
@@ -3,6 +3,7 @@
 cd $(dirname "$0")/../..
 
 git -C starwars-server pull || git clone https://github.com/apollographql/starwars-server
+git -C starwars-server checkout 0.x-legacy-optionals
 
 cd starwars-server
 
