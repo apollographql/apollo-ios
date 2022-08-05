6f67f488c10f73fd14b9d272905ed63d616b71a3
Anthony Miller <anthonymdev@gmail.com>
2022-08-05 15:00:45 -0700

    Fix Subscription API linkage in Xcode project

--- Sources/SubscriptionAPI/Schema/Objects/Subscription.swift
+++
@@ -1,8 +0,0 @@
-// @generated
-// This file was automatically generated and should not be edited.
-
-import ApolloAPI
-
-public final class Subscription: Object {
-  override public class var __typename: StaticString { "Subscription" }
-}
