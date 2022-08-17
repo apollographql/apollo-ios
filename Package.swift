11285173d6677c5dc2986f242f16a57b2a7e726b
Calvin Cestari <calvin.cestari@apollographql.com>
2022-08-17 18:07:53 +0200

    Add CodegenCLI to apollo-ios Swift package

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,39 +12,30 @@ let package = Package(
     .watchOS(.v5)
   ],
   products: [
-    .library(
-      name: "Apollo",
-      targets: ["Apollo"]),
-    .library(
-      name: "ApolloAPI",
-      targets: ["ApolloAPI"]),
-    .library(
-      name: "Apollo-Dynamic",
-      type: .dynamic,
-      targets: ["Apollo"]),
-    .library(
-      name: "ApolloCodegenLib",
-      targets: ["ApolloCodegenLib"]),
-    .library(
-      name: "ApolloSQLite",
-      targets: ["ApolloSQLite"]),
-    .library(
-      name: "ApolloWebSocket",
-      targets: ["ApolloWebSocket"]),
-    .library(
-      name: "ApolloTestSupport",
-      targets: ["ApolloTestSupport"]),
+    .library(name: "Apollo", targets: ["Apollo"]),
+    .library(name: "ApolloAPI", targets: ["ApolloAPI"]),
+    .library(name: "Apollo-Dynamic", type: .dynamic, targets: ["Apollo"]),
+    .library(name: "ApolloCodegenLib", targets: ["ApolloCodegenLib"]),
+    .library(name: "ApolloSQLite", targets: ["ApolloSQLite"]),
+    .library(name: "ApolloWebSocket", targets: ["ApolloWebSocket"]),
+    .library(name: "ApolloTestSupport", targets: ["ApolloTestSupport"]),
   ],
   dependencies: [
     .package(
       url: "https://github.com/stephencelis/SQLite.swift.git",
-        .upToNextMinor(from: "0.13.1")),
+      .upToNextMinor(from: "0.13.1")),
     .package(
       url: "https://github.com/mattt/InflectorKit",
       .upToNextMinor(from: "1.0.0")),
     .package(
       url: "https://github.com/apple/swift-collections",
-      .upToNextMajor(from: "1.0.0"))
+      .upToNextMajor(from: "1.0.0")),
+    .package(
+      url: "https://github.com/apple/swift-argument-parser.git", 
+      .upToNextMajor(from: "1.1.2")),
+    .package(
+      url: "https://github.com/Quick/Nimble.git", 
+      .upToNextMajor(from: "10.0.0")),
   ],
   targets: [
     .target(
@@ -97,5 +88,22 @@ let package = Package(
       exclude: [
         "Info.plist"
       ]),
+    .executableTarget(
+      name: "apollo-ios-cli",
+      dependencies: [
+        "ApolloCodegenLib",
+        .product(name: "ArgumentParser", package: "swift-argument-parser"),
+      ],
+      path: "./Sources/CodegenCLI/",
+      exclude: [
+        "README.md"
+      ]),
+    .testTarget(
+      name: "CodegenCLITests",
+      dependencies: [
+        "apollo-ios-cli",
+        "Nimble",
+      ],
+      path: "./Tests/CodegenCLI/"),
   ]
 )
