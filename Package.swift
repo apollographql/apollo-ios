b12d2950d6ef7099a78dba887ea4279a5df3b962
Anthony Miller <anthonymdev@gmail.com>
2022-08-22 15:31:41 -0700

    Rebase on CodegenCLI changes and clean up CodegenCLI package settings

--- Package.swift
+++ Package.swift
@@ -93,6 +93,7 @@ let package = Package(
         .product(name: "ArgumentParser", package: "swift-argument-parser"),
       ],
       exclude: [
+        "Info.plist",
         "README.md"
       ]),
   ]
