a9fc5730af97d90c1c2d9ce7c136219a475e793d
Calvin Cestari <calvin.cestari@apollographql.com>
2022-09-13 13:15:00 -0700

    Uses installed node latest version

--- scripts/install-node-v12.sh
+++ scripts/install-node-v12.sh
@@ -5,4 +5,4 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 echo 'export NVM_DIR="$HOME/.nvm"' >> $BASH_ENV
 echo '[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
 echo nvm install v12.22.10 >> $BASH_ENV
-echo nvm use v16.15.1 >> $BASH_ENV
+echo nvm use v16.17.0 >> $BASH_ENV
