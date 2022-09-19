7a734435100fe3442d5c5fb56912a9ab36ceb260
Calvin Cestari <calvincestari@users.noreply.github.com>
2022-09-19 21:28:47 +0200

    Add SwiftPM Plugin Permission Definitions (#2492)

--- Plugins/ApolloCodegenPlugin-Fetch/ApolloCodegenPluginFetch.swift
+++ Plugins/ApolloCodegenPlugin-Fetch/ApolloCodegenPluginFetch.swift
@@ -6,7 +6,6 @@ import PackagePlugin
     let process = Process()
     process.executableURL = try context.codegenExecutable
     process.arguments = ["fetch-schema"] + arguments
-    process.terminationHandler = Process.HandleErrorTermination
 
     try process.run()
     process.waitUntilExit()
