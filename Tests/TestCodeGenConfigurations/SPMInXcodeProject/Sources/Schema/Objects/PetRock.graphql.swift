5d7a54a5ace778a33b3be30c8463468368fa7252
Anthony Miller <anthonymdev@gmail.com>
2022-09-30 00:26:00 -0500

    Fix Test Mock bugs (#2532)

---
+++ Tests/TestCodeGenConfigurations/SPMInXcodeProject/Sources/Schema/Objects/PetRock.graphql.swift
@@ -0,0 +1,11 @@
+// @generated
+// This file was automatically generated and should not be edited.
+
+import ApolloAPI
+
+public extension Objects {
+  static let PetRock = Object(
+    typename: "PetRock",
+    implementedInterfaces: [Interfaces.Pet.self]
+  )
+}
