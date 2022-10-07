b9669774fb991ad75548bff84415e5fcc753df5c
Calvin Cestari <calvin.cestari@apollographql.com>
2022-10-07 12:51:36 -0700

    Updates package description to use local dependency path

--- Tests/CodegenCLITests/swiftpm-test/Package.swift
+++ Tests/CodegenCLITests/swiftpm-test/Package.swift
@@ -4,21 +4,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "swiftpm-test",
-    platforms: [.macOS(.v10_15)],
-    products: [
-        .library(
-            name: "swiftpm-test",
-            targets: ["swiftpm-test"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apollographql/apollo-ios.git", branch: "fix/xcode-spm-plugin"),
-    ],
-    targets: [
-        .target(
-            name: "swiftpm-test",
-            dependencies: [
-              .product(name: "Apollo", package: "apollo-ios"),
-            ]),
-    ]
+  name: "swiftpm-test",
+  platforms: [.macOS(.v10_15)],
+  products: [
+    .library(
+      name: "swiftpm-test",
+      targets: ["swiftpm-test"]),
+  ],
+  dependencies: [
+    .package(name: "apollo-ios", path: "../../.."),
+  ],
+  targets: [
+    .target(
+      name: "swiftpm-test",
+      dependencies: [
+        .product(name: "Apollo", package: "apollo-ios"),
+      ]),
+  ]
 )
