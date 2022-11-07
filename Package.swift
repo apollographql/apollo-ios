e486de65b95ad69f49870c34fbb7f4ebc4a5fd61
Calvin Cestari <calvin.cestari@apollographql.com>
2022-11-07 13:54:32 -0800

    Remove SwiftPM plugins

--- Package.swift
+++ Package.swift
@@ -20,9 +20,6 @@ let package = Package(
     .library(name: "ApolloWebSocket", targets: ["ApolloWebSocket"]),
     .library(name: "ApolloTestSupport", targets: ["ApolloTestSupport"]),
     .executable(name: "apollo-ios-cli", targets: ["apollo-ios-cli"]),
-    .plugin(name: "ApolloCodegenPlugin-Initialize", targets: ["ApolloCodegenPlugin-Initialize"]),
-    .plugin(name: "ApolloCodegenPlugin-Fetch", targets: ["ApolloCodegenPlugin-Fetch"]),
-    .plugin(name: "ApolloCodegenPlugin-Generate", targets: ["ApolloCodegenPlugin-Generate"]),
   ],
   dependencies: [
     .package(
@@ -104,42 +101,6 @@ let package = Package(
       ],
       exclude: [
         "Info.plist",
-      ]),
-    .plugin(
-      name: "ApolloCodegenPlugin-Initialize",
-      capability: .command(
-        intent: .custom(
-          verb: "apollo-initialize-codegen-config",
-          description: "Initialize a new code generation configuration with defaults."),
-        permissions: [
-          .writeToPackageDirectory(reason: "Adds a codegen JSON configuration file.")
-        ]),
-      dependencies: [
-        "apollo-ios-cli"
-      ]),
-    .plugin(
-      name: "ApolloCodegenPlugin-Fetch",
-      capability: .command(
-        intent: .custom(
-          verb: "apollo-fetch-schema",
-          description: "Download a GraphQL schema from the Apollo Registry or via GraphQL introspection."),
-        permissions: [
-          .writeToPackageDirectory(reason: "Downloads the GraphQL schema to a file.")
-        ]),
-      dependencies: [
-        "apollo-ios-cli"
-      ]),
-    .plugin(
-      name: "ApolloCodegenPlugin-Generate",
-      capability: .command(
-        intent: .custom(
-          verb: "apollo-generate",
-          description: "Generate Swift code for the configured GraphQL schema and operations."),
-        permissions: [
-          .writeToPackageDirectory(reason: "Generates Swift files for the schema and operations.")
-        ]),
-      dependencies: [
-        "apollo-ios-cli"
       ]),
   ]
 )
