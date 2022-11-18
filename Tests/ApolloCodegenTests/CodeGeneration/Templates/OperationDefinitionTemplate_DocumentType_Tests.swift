14bcb4a9e1df48b81e3b0f684a360bf37615f77d
Calvin Cestari <calvin.cestari@apollographql.com>
2022-11-18 12:05:54 -0800

    Adds namespace to DocumentType

--- Tests/ApolloCodegenTests/CodeGeneration/Templates/OperationDefinitionTemplate_DocumentType_Tests.swift
+++ Tests/ApolloCodegenTests/CodeGeneration/Templates/OperationDefinitionTemplate_DocumentType_Tests.swift
@@ -29,8 +29,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
       try XCTUnwrap(definition),
       identifier: operationIdentifier,
       fragments: referencedFragments ?? [],
-      apq: try XCTUnwrap(config.options.apqs),
-      queryStringLiteralFormat: try XCTUnwrap(config.options.queryStringLiteralFormat)
+      config: ApolloCodegen.ConfigurationContext(config: config)
     ).description
   }
 
@@ -53,7 +52,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         ""\"
         query NameQuery {
@@ -84,7 +83,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         "query NameQuery {  name}"
       ))
@@ -116,7 +115,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         ""\"
         query NameQuery {
@@ -153,7 +152,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         "query NameQuery {  ...NameFragment}",
         fragments: [NameFragment.self]
@@ -186,7 +185,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         "query NameQuery {  ...nameFragment}",
         fragments: [NameFragment.self]
@@ -227,7 +226,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         ""\"
         query NameQuery {
@@ -276,7 +275,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .notPersisted(
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
       definition: .init(
         "query NameQuery {  ...Fragment1  ...Fragment2  ...Fragment3  ...Fragment4  ...FragmentWithLongName1234123412341234123412341234}",
         fragments: [Fragment1.self, Fragment2.self, Fragment3.self, Fragment4.self, FragmentWithLongName1234123412341234123412341234.self]
@@ -306,7 +305,7 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .automaticallyPersisted(
+    public static let document: ApolloAPI.DocumentType = .automaticallyPersisted(
       operationIdentifier: "1ec89997a185c50bacc5f62ad41f27f3070f4a950d72e4a1510a4c64160812d5",
       definition: .init(
         ""\"
@@ -340,10 +339,56 @@ class OperationDefinitionTemplate_DocumentType_Tests: XCTestCase {
     // then
     let expected =
     """
-    public static let document: DocumentType = .persistedOperationsOnly(
+    public static let document: ApolloAPI.DocumentType = .persistedOperationsOnly(
       operationIdentifier: "1ec89997a185c50bacc5f62ad41f27f3070f4a950d72e4a1510a4c64160812d5"
     )
     """
     expect(actual).to(equalLineByLine(expected))
+  }
+
+  func test__generate__givenCocoapodsCompatibleImportStatements_true_shouldUseCorrectNamespace() throws {
+    // given
+    definition.source =
+    """
+    query NameQuery {
+      name
+    }
+    """
+    config = .mock(options: .init(
+      cocoapodsCompatibleImportStatements: true
+    ))
+
+    // when
+    let actual = try renderDocumentType()
+
+    // then
+    let expected =
+    """
+    public static let document: Apollo.DocumentType = .notPersisted(
+    """
+    expect(actual).to(equalLineByLine(expected, ignoringExtraLines: true))
+  }
+
+  func test__generate__givenCocoapodsCompatibleImportStatements_false_shouldUseCorrectNamespace() throws {
+    // given
+    definition.source =
+    """
+    query NameQuery {
+      name
+    }
+    """
+    config = .mock(options: .init(
+      cocoapodsCompatibleImportStatements: false
+    ))
+
+    // when
+    let actual = try renderDocumentType()
+
+    // then
+    let expected =
+    """
+    public static let document: ApolloAPI.DocumentType = .notPersisted(
+    """
+    expect(actual).to(equalLineByLine(expected, ignoringExtraLines: true))
   }
 }
