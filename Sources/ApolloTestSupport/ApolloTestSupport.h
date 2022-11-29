c33596c6c328ad6b99ef5a1659159aaa2106add0
Calvin Cestari <calvin.cestari@apollographql.com>
2022-11-28 21:03:17 -0800

    Removes unnecessary versioning files

--- Sources/ApolloTestSupport/ApolloTestSupport.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for ApolloTestSupport.
-FOUNDATION_EXPORT double ApolloTestSupportVersionNumber;
-
-//! Project version string for ApolloTestSupport.
-FOUNDATION_EXPORT const unsigned char ApolloTestSupportVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <ApolloTestSupport/PublicHeader.h>
