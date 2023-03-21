40ff9b4dcfe96e19c6c9647bb675bbeb5ff55722
Anthony Miller <anthonymdev@gmail.com>
2023-03-21 12:03:01 -0700

    Add simple performance test for type case conversion

---
+++ Tests/ApolloPerformanceTests/TypeCaseConversionPerformanceTests.swift
@@ -0,0 +1,27 @@
+import Foundation
+import XCTest
+@testable import Apollo
+import ApolloAPI
+import ApolloInternalTestHelpers
+import AnimalKingdomAPI
+
+class TypeCaseConversionPerformanceTests: XCTestCase {
+
+  func testPerformance_typeConversion_checkTypeConformsToInterface() {
+    let animal = AllAnimalsQuery.Data.AllAnimal.AsDog(
+      favoriteToy: "Milk Bone",
+      height: .init(feet: 3, meters: 1, relativeSize: .case(.small), centimeters: 100),
+      species: "Canine",
+      predators: [],
+      bodyTemperature: 98
+    ).asRootEntityType
+
+    measure {
+      for _ in 0..<1_000 {
+        let asDog = animal.asDog
+        XCTAssertNotNil(asDog)
+      }
+    }
+  }
+
+}
