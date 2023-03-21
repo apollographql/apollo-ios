34930a71da34aad21b13b4f18e67ae34bdf71fb0
Anthony Miller <anthonymdev@gmail.com>
2023-03-21 12:52:01 -0700

    Make ParsingPerformanceTests work again

--- Tests/ApolloPerformanceTests/TypeCaseConversionPerformanceTests.swift
+++ Tests/ApolloPerformanceTests/TypeCaseConversionPerformanceTests.swift
@@ -24,4 +24,21 @@ class TypeCaseConversionPerformanceTests: XCTestCase {
     }
   }
 
+//  func testPerformance_typeConversion_checkTypeIsInUnion() {
+//    let animal = AllAnimalsQuery.Data.AllAnimal.AsDog(
+//      favoriteToy: "Milk Bone",
+//      height: .init(feet: 3, meters: 1, relativeSize: .case(.small), centimeters: 100),
+//      species: "Canine",
+//      predators: [],
+//      bodyTemperature: 98
+//    ).asRootEntityType
+//
+//    measure {
+//      for _ in 0..<1_000 {
+//        let asDog = animal.asDog
+//        XCTAssertNotNil(asDog)
+//      }
+//    }
+//  }
+
 }
