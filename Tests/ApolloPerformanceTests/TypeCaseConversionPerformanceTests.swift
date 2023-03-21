3d64ed8859e4d6db87e2802befa4849d2dce4e49
Anthony Miller <anthonymdev@gmail.com>
2023-03-21 13:09:45 -0700

    Add inclusion condition conversion performance test

--- Tests/ApolloPerformanceTests/TypeCaseConversionPerformanceTests.swift
+++ Tests/ApolloPerformanceTests/TypeCaseConversionPerformanceTests.swift
@@ -24,21 +24,20 @@ class TypeCaseConversionPerformanceTests: XCTestCase {
     }
   }
 
-//  func testPerformance_typeConversion_checkTypeIsInUnion() {
-//    let animal = AllAnimalsQuery.Data.AllAnimal.AsDog(
-//      favoriteToy: "Milk Bone",
-//      height: .init(feet: 3, meters: 1, relativeSize: .case(.small), centimeters: 100),
-//      species: "Canine",
-//      predators: [],
-//      bodyTemperature: 98
-//    ).asRootEntityType
-//
-//    measure {
-//      for _ in 0..<1_000 {
-//        let asDog = animal.asDog
-//        XCTAssertNotNil(asDog)
-//      }
-//    }
-//  }
+  func testPerformance_typeConversion_inclusionCondition() {
+    let animal = AllAnimalsIncludeSkipQuery.Data.AllAnimal.IfNotSkipHeightInMeters(
+      __typename: "Dog",
+      height: .init(feet: 3,  inches: 36, meters: 1),
+      species: "Canine",
+      predators: []
+    ).asRootEntityType
+
+    measure {
+      for _ in 0..<1_000 {
+        let asConditional = animal.ifNotSkipHeightInMeters
+        XCTAssertNotNil(asConditional)
+      }
+    }
+  }
 
 }
