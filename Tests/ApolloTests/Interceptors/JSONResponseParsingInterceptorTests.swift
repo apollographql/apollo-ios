77c8e3e544cbfffda1020b3089b9b02e79cfb716
tokizo <tokizuoh@gmail.com>
2023-03-30 01:33:39 +0900

    test: Add JSONResponseParsingInterceptor test for missing response (#2903)

--- Tests/ApolloTests/Interceptors/JSONResponseParsingInterceptorTests.swift
+++ Tests/ApolloTests/Interceptors/JSONResponseParsingInterceptorTests.swift
@@ -4,28 +4,50 @@ import ApolloInternalTestHelpers
 import XCTest
 
 class JSONResponseParsingInterceptorTests: XCTestCase {
+  func testJSONResponseParsingInterceptorFailsWithImproperlyOrderedCalls() {
+    let provider = MockInterceptorProvider([
+      JSONResponseParsingInterceptor()
+    ])
 
-  func testJSONResponseParsingInterceptorFailsWithEmptyData() {
-    class TestProvider: InterceptorProvider {
-      let mockClient: MockURLSessionClient = {
-        let client = MockURLSessionClient()
-        client.response = HTTPURLResponse(url: TestURL.mockServer.url,
-                                          statusCode: 200,
-                                          httpVersion: nil,
-                                          headerFields: nil)
-        client.data = Data()
-        return client
-      }()
+    let network = RequestChainNetworkTransport(interceptorProvider: provider,
+                                               endpointURL: TestURL.mockServer.url)
 
-      func interceptors<Operation: GraphQLOperation>(for operation: Operation) -> [ApolloInterceptor] {
-        [
-          NetworkFetchInterceptor(client: self.mockClient),
-          JSONResponseParsingInterceptor(),
-        ]
+    let expectation = self.expectation(description: "Request sent")
+
+    _ = network.send(operation: MockQuery.mock()) { result in
+      defer {
+        expectation.fulfill()
+      }
+
+      switch result {
+      case .success:
+        XCTFail("This should not have succeeded")
+      case .failure(let error):
+        switch error {
+        case JSONResponseParsingInterceptor.JSONResponseParsingError.noResponseToParse:
+          // This is what we want
+          break
+        default:
+          XCTFail("Unexpected error type: \(error.localizedDescription)")
+        }
       }
     }
 
-    let network = RequestChainNetworkTransport(interceptorProvider: TestProvider(),
+    self.wait(for: [expectation], timeout: 1)
+  }
+
+  func testJSONResponseParsingInterceptorFailsWithEmptyData() {
+    let client = MockURLSessionClient(
+      response: .mock(),
+      data: Data()
+    )
+    
+    let provider = MockInterceptorProvider([
+      NetworkFetchInterceptor(client: client),
+      JSONResponseParsingInterceptor(),
+    ])
+
+    let network = RequestChainNetworkTransport(interceptorProvider: provider,
                                                endpointURL: TestURL.mockServer.url)
 
     let expectation = self.expectation(description: "Request sent")
