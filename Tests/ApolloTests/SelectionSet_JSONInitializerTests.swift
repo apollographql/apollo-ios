c770ff2c0cd6afc40925cefb8e86641133ae5aaf
Anthony Miller <anthonymdev@gmail.com>
2023-03-30 13:17:03 -0700

    WIP: Test for merged only selection set JSON initialization

--- Tests/ApolloTests/SelectionSet_JSONInitializerTests.swift
+++ Tests/ApolloTests/SelectionSet_JSONInitializerTests.swift
@@ -124,4 +124,124 @@ class SelectionSet_JSONInitializerTests: XCTestCase {
     expect(dataWithNoName.name).to(beNil())
   }
 
+  // MARK: - Merged Only Selection Set Tests
+
+  /// Confirms bug fix for issue [#2915](https://github.com/apollographql/apollo-ios/issues/2915).
+  func test__initFromJSON__givenMergedOnlyNestedSelectionSet_withTypeCase_canConvertToTypeCase() throws {
+    struct Types {
+      static let Character = Interface(name: "Character")
+      static let Hero = Interface(name: "Hero")
+      static let Human = Object(typename: "Human", implementedInterfaces: [Character.self, Hero.self])
+    }
+
+    MockSchemaMetadata.stub_objectTypeForTypeName = {
+      switch $0 {
+      case "Human": return Types.Human
+      default: XCTFail(); return nil
+      }
+    }
+
+    class Character: MockSelectionSet {
+      typealias Schema = MockSchemaMetadata
+
+      override class var __parentType: ParentType { Types.Character }
+      override class var __selections: [Selection] {[
+        .field("__typename", String.self),
+        .field("friend", Friend.self),
+        .inlineFragment(AsHero.self),
+        .inlineFragment(AsHuman.self),
+      ]}
+
+      var friend: Friend { __data["friend"] }
+      var asHero: AsHero? { _asInlineFragment() }
+      var asHuman: AsHuman? { _asInlineFragment() }
+
+      class Friend: MockSelectionSet {
+        typealias Schema = MockSchemaMetadata
+
+        override class var __parentType: ParentType { Types.Character }
+        override class var __selections: [Selection] {[
+          .field("__typename", String.self),
+          .inlineFragment(AsHuman.self),
+        ]}
+
+        var asHuman: AsHuman? { _asInlineFragment() }
+
+        class AsHuman: ConcreteMockTypeCase<Character.Friend> {
+          typealias Schema = MockSchemaMetadata
+
+          override class var __parentType: ParentType { Types.Human }
+          override class var __selections: [Selection] {[
+            .field("name", String.self),
+          ]}
+
+          var name: String? { __data["name"] }
+        }
+      }
+
+      class AsHero: ConcreteMockTypeCase<Character> {
+        typealias Schema = MockSchemaMetadata
+
+        override class var __parentType: ParentType { Types.Hero }
+        override class var __selections: [Selection] {[
+          .field("friend", Friend.self),
+        ]}
+
+        var friend: Friend { __data["friend"] }
+
+        class Friend: MockSelectionSet {
+          typealias Schema = MockSchemaMetadata
+
+          override class var __parentType: ParentType { Types.Character }
+          override class var __selections: [Selection] {[
+            .field("heroName", String.self),
+          ]}
+
+          var heroName: String? { __data["heroName"] }
+        }
+      }
+
+      class AsHuman: MockTypeCase {
+        typealias Schema = MockSchemaMetadata
+
+        override class var __parentType: ParentType { Types.Human }
+        override class var __selections: [Selection] {[
+          .field("name", String.self),
+        ]}
+
+        var name: String? { __data["name"] }
+        var friend: Friend { __data["friend"] }
+
+        class Friend: MockSelectionSet {
+          typealias Schema = MockSchemaMetadata
+
+          override class var __parentType: ParentType { Types.Character }
+
+          var heroName: String? { __data["heroName"] }
+          var asHuman: AsHuman? { _asInlineFragment() }
+
+          class AsHuman: ConcreteMockTypeCase<Character.AsHuman.Friend> {
+            typealias Schema = MockSchemaMetadata
+
+            override class var __parentType: ParentType { Types.Human }
+
+            var name: String? { __data["name"] }
+            var heroName: String? { __data["heroName"] }
+          }
+        }
+      }
+    }
+
+    let jsonObject: JSONObject = [
+      "__typename": "Human", "name": "Anikan", "friend": [
+        "__typename": "Human",
+        "name": "Han",
+        "heroName": "Han Solo"
+      ]
+    ]
+
+    let data = try Character(data: jsonObject)
+    expect(data.asHuman?.friend.asHuman).toNot(beNil())
+    expect(data.asHuman?.friend.asHuman?.heroName).to(equal("Han Solo"))
+  }
 }
