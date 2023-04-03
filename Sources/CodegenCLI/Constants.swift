086881a9b3b4c7966a44809e8e709beca932c201
Anthony Miller <anthonymdev@gmail.com>
2023-04-03 14:05:54 -0700

    Update change log and bump version

--- Sources/CodegenCLI/Constants.swift
+++ Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "1.1.0-beta.1"
+  public static let CLIVersion: String = "1.1.0"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
