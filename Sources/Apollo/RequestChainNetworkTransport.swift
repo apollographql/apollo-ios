3aa8812693c0366db6890271fad11e2ea2ab6298
RobertDresler <40290767+RobertDresler@users.noreply.github.com>
2023-04-11 22:03:59 +0200

    feat: Add injecting `additionalErrorHandler` for upload operations to `RequestChainNetworkTransport` (#2948)
    Co-authored-by: Calvin Cestari <calvin.cestari@gmail.com>

--- Sources/Apollo/RequestChainNetworkTransport.swift
+++ Sources/Apollo/RequestChainNetworkTransport.swift
@@ -107,9 +107,7 @@ open class RequestChainNetworkTransport: NetworkTransport {
     callbackQueue: DispatchQueue = .main,
     completionHandler: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) -> Cancellable {
     
-    let interceptors = self.interceptorProvider.interceptors(for: operation)
-    let chain = InterceptorRequestChain(interceptors: interceptors, callbackQueue: callbackQueue)
-    chain.additionalErrorHandler = self.interceptorProvider.additionalErrorInterceptor(for: operation)
+    let chain = makeChain(operation: operation, callbackQueue: callbackQueue)
     let request = self.constructRequest(for: operation,
                                         cachePolicy: cachePolicy,
                                         contextIdentifier: contextIdentifier)
@@ -124,6 +122,17 @@ open class RequestChainNetworkTransport: NetworkTransport {
     chain.kickoff(request: request, completion: completionHandler)
     return chain
   }
+
+  private func makeChain<Operation: GraphQLOperation>(
+    operation: Operation,
+    callbackQueue: DispatchQueue = .main
+  ) -> RequestChain {
+    let interceptors = self.interceptorProvider.interceptors(for: operation)
+    let chain = InterceptorRequestChain(interceptors: interceptors, callbackQueue: callbackQueue)
+    chain.additionalErrorHandler = self.interceptorProvider.additionalErrorInterceptor(for: operation)
+    return chain
+  }
+  
 }
 
 extension RequestChainNetworkTransport: UploadingNetworkTransport {
@@ -159,9 +168,7 @@ extension RequestChainNetworkTransport: UploadingNetworkTransport {
     completionHandler: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) -> Cancellable {
     
     let request = self.constructUploadRequest(for: operation, with: files)
-    let interceptors = self.interceptorProvider.interceptors(for: operation)
-    let chain = InterceptorRequestChain(interceptors: interceptors, callbackQueue: callbackQueue)
-    
+    let chain = makeChain(operation: operation, callbackQueue: callbackQueue)
     chain.kickoff(request: request, completion: completionHandler)
     return chain
   }
