ba5200e6a1d33accd4187bda6dc77bc51c250eee
Robert Dresler <robertdreslerjr@gmail.com>
2023-04-11 09:44:53 +0200

    FIX: Add injecting additionalErrorHandler for upload operations to RequestChainNetworkTransport

--- Sources/Apollo/RequestChainNetworkTransport.swift
+++ Sources/Apollo/RequestChainNetworkTransport.swift
@@ -107,9 +107,7 @@ open class RequestChainNetworkTransport: NetworkTransport {
     callbackQueue: DispatchQueue = .main,
     completionHandler: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) -> Cancellable {
     
-    let interceptors = self.interceptorProvider.interceptors(for: operation)
-    let chain = InterceptorRequestChain(interceptors: interceptors, callbackQueue: callbackQueue)
-    chain.additionalErrorHandler = self.interceptorProvider.additionalErrorInterceptor(for: operation)
+    let chain = makeChain(operation: operation, callbackQueue: callbackQueue)
     let request = self.constructRequest(for: operation,
                                         cachePolicy: cachePolicy,
                                         contextIdentifier: contextIdentifier)
@@ -159,10 +157,18 @@ extension RequestChainNetworkTransport: UploadingNetworkTransport {
     completionHandler: @escaping (Result<GraphQLResult<Operation.Data>, Error>) -> Void) -> Cancellable {
     
     let request = self.constructUploadRequest(for: operation, with: files)
+    let chain = makeChain(operation: operation, callbackQueue: callbackQueue)
+    chain.kickoff(request: request, completion: completionHandler)
+    return chain
+  }
+
+  public func makeChain<Operation: GraphQLOperation>(
+    operation: Operation,
+    callbackQueue: DispatchQueue = .main
+  ) -> RequestChain {
     let interceptors = self.interceptorProvider.interceptors(for: operation)
     let chain = InterceptorRequestChain(interceptors: interceptors, callbackQueue: callbackQueue)
-    
-    chain.kickoff(request: request, completion: completionHandler)
+    chain.additionalErrorHandler = self.interceptorProvider.additionalErrorInterceptor(for: operation)
     return chain
   }
 }
