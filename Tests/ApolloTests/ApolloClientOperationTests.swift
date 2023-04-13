a625075ebb89805c5ee1401034f4f1148b4306cd
tokizuoh <tokizuoh@gmail.com>
2023-04-14 00:42:27 +0900

    add test: test__performMutation_givenPublishResultToStore_true_publishResultsToStore
    when the publishResultToStore argument of perform is true, the result is cached.

--- Tests/ApolloTests/ApolloClientOperationTests.swift
+++ Tests/ApolloTests/ApolloClientOperationTests.swift
@@ -41,6 +41,57 @@ final class ApolloClientOperationTests: XCTestCase {
     }
   }
 
+  func test__performMutation_givenPublishResultToStore_true_publishResultsToStore() throws {
+    // given
+    class GivenSelectionSet: MockSelectionSet {
+      override class var __selections: [Selection] { [
+        .field("createReview", CreateReview.self)
+      ] }
+
+      class CreateReview: MockSelectionSet {
+        override class var __selections: [Selection] { [
+          .field("__typename", String.self),
+          .field("stars", Int.self),
+          .field("commentary", String?.self)
+        ] }
+      }
+    }
+    let mutation = MockMutation<GivenSelectionSet>()
+    let resultObserver = self.makeResultObserver(for: mutation)
+
+    let serverRequestExpectation = self.server.expect(MockMutation<GivenSelectionSet>.self) { _ in
+      [
+        "data": [
+          "createReview": [
+            "__typename": "Review",
+            "stars": 3,
+            "commentary": ""
+          ]
+        ]
+      ]
+    }
+
+    let performResultFromServerExpectation =
+      resultObserver.expectation(description: "Mutation was successful") { result in
+        switch (result) {
+        case .success:
+          break
+        case let .failure(error):
+          fail("Unexpected failure! \(error)")
+        }
+      }
+
+    // when
+    self.client.perform(mutation: mutation,
+                        publishResultToStore: true,
+                        resultHandler: resultObserver.handler)
+
+    self.wait(for: [serverRequestExpectation, performResultFromServerExpectation], timeout: 0.2)
+
+    // then
+    expect(self.store.publishedRecordSets).notTo(beEmpty())
+  }
+
   func test__performMutation_givenPublishResultToStore_false_doesNotPublishResultsToStore() throws {
     // given
     class GivenSelectionSet: MockSelectionSet {
