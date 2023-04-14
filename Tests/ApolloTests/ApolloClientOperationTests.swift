b8e9bae9bd53b8c2c00c51879a0e2b3d072c6856
tokizo <tokizuoh@gmail.com>
2023-04-15 02:07:36 +0900

    test: Apollo client operation cache test when `publishResultToStore` is true (#2953)

--- Tests/ApolloTests/ApolloClientOperationTests.swift
+++ Tests/ApolloTests/ApolloClientOperationTests.swift
@@ -41,34 +41,80 @@ final class ApolloClientOperationTests: XCTestCase {
     }
   }
 
-  func test__performMutation_givenPublishResultToStore_false_doesNotPublishResultsToStore() throws {
-    // given
-    class GivenSelectionSet: MockSelectionSet {
-      override class var __selections: [Selection] { [
-        .field("createReview", CreateReview.self)
-      ] }
+  // given
+  class GivenSelectionSet: MockSelectionSet {
+    override class var __selections: [Selection] { [
+      .field("createReview", CreateReview.self)
+    ] }
 
-      class CreateReview: MockSelectionSet {
-        override class var __selections: [Selection] { [
-          .field("__typename", String.self),
-          .field("stars", Int.self),
-          .field("commentary", String?.self)
-        ] }
-      }
+    class CreateReview: MockSelectionSet {
+      override class var __selections: [Selection] { [
+        .field("__typename", String.self),
+        .field("stars", Int.self),
+        .field("commentary", String?.self)
+      ] }
     }
+  }
+
+  let jsonObject: JSONObject = [
+    "data": [
+      "createReview": [
+        "__typename": "Review",
+        "stars": 3,
+        "commentary": ""
+      ]
+    ]
+  ]
+
+  func test__performMutation_givenPublishResultToStore_true_publishResultsToStore() throws {
     let mutation = MockMutation<GivenSelectionSet>()
     let resultObserver = self.makeResultObserver(for: mutation)
 
-    let serverRequestExpectation = self.server.expect(MockMutation<GivenSelectionSet>.self) { _ in
-      [
-        "data": [
-          "createReview": [
-            "__typename": "Review",
-            "stars": 3,
-            "commentary": ""
-          ]
-        ]
-      ]
+    let serverRequestExpectation = server.expect(MockMutation<GivenSelectionSet>.self) { _ in
+      self.jsonObject
+    }
+
+    let performResultFromServerExpectation =
+      resultObserver.expectation(description: "Mutation was successful") { result in
+        switch (result) {
+        case .success:
+          break
+        case let .failure(error):
+          fail("Unexpected failure! \(error)")
+        }
+      }
+
+    // when
+    self.client.perform(mutation: mutation,
+                        publishResultToStore: true,
+                        resultHandler: resultObserver.handler)
+
+    self.wait(for: [serverRequestExpectation, performResultFromServerExpectation], timeout: 0.2)
+
+    // then
+    expect(self.store.publishedRecordSets.count).to(equal(1))
+    
+    let actual = self.store.publishedRecordSets[0]
+    expect(actual["MUTATION_ROOT"]).to(equal(
+      Record(key: "MUTATION_ROOT", [
+        "createReview": CacheReference("MUTATION_ROOT.createReview")
+      ])
+    ))
+    expect(actual["MUTATION_ROOT.createReview"]).to(equal(
+      Record(key: "MUTATION_ROOT.createReview", [
+        "__typename": "Review",
+        "stars": 3,
+        "commentary": ""
+      ])
+    ))
+  }
+
+  func test__performMutation_givenPublishResultToStore_false_doesNotPublishResultsToStore() throws {
+    let mutation = MockMutation<GivenSelectionSet>()
+    let resultObserver = self.makeResultObserver(for: mutation)
+
+    let serverRequestExpectation = server.expect(MockMutation<GivenSelectionSet>.self) { _ in
+      self.jsonObject
     }
 
     let performResultFromServerExpectation =
