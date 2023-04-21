d25f66d3e3f25af57ce9a9df2311cc0afe2c39e0
Calvin Cestari <calvin.cestari@apollographql.com>
2023-04-21 10:15:19 -0700

    Adds multipart performance test with baseline

--- Tests/ApolloPerformanceTests/ParsingPerformanceTests.swift
+++ Tests/ApolloPerformanceTests/ParsingPerformanceTests.swift
@@ -35,6 +35,102 @@ class ParsingPerformanceTests: XCTestCase {
     }
   }
 
+  func testMultipartResponseParsingInterceptor() throws {
+    class DataCaptureRequestChain<Operation: GraphQLOperation>: RequestChain {
+      var isCancelled: Bool = false
+      var completion: (Data?) -> Void
+
+      init(
+        _ completion: @escaping (Data?) -> Void
+      ) {
+        self.completion = completion
+      }
+
+      func kickoff<Operation>(
+        request: Apollo.HTTPRequest<Operation>,
+        completion: @escaping (Result<Apollo.GraphQLResult<Operation.Data>, Error>) -> Void
+      ) {}
+
+      func proceedAsync<Operation>(
+        request: Apollo.HTTPRequest<Operation>,
+        response: Apollo.HTTPResponse<Operation>?,
+        completion: @escaping (Result<Apollo.GraphQLResult<Operation.Data>, Error>) -> Void
+      ) {
+        self.completion(response?.rawData)
+      }
+
+      func cancel() {}
+
+      func retry<Operation>(
+        request: Apollo.HTTPRequest<Operation>,
+        completion: @escaping (Result<Apollo.GraphQLResult<Operation.Data>, Error>) -> Void
+      ) {}
+
+      func handleErrorAsync<Operation>(
+        _ error: Error,
+        request: Apollo.HTTPRequest<Operation>,
+        response: Apollo.HTTPResponse<Operation>?,
+        completion: @escaping (Result<Apollo.GraphQLResult<Operation.Data>, Error>) -> Void
+      ) {}
+
+      func returnValueAsync<Operation>(
+        for request: Apollo.HTTPRequest<Operation>,
+        value: Apollo.GraphQLResult<Operation.Data>,
+        completion: @escaping (Result<Apollo.GraphQLResult<Operation.Data>, Error>) -> Void
+      ) {}
+    }
+
+    var rawData: String = ""
+    for _ in 0..<100 {
+      rawData.append("""
+        --graphql
+        content-type: application/json
+
+        {
+          "payload": {
+            "data": {
+              "ticker": 1
+            }
+          }
+        }
+        --graphql
+        """)
+    }
+
+    let operation = MockSubscription.mock()
+    let request = JSONRequest(
+      operation: operation,
+      graphQLEndpoint: TestURL.mockServer.url,
+      clientName: "ApolloPerformanceTest",
+      clientVersion: "0",
+      additionalHeaders: [
+        "Accept" : "multipart/mixed;boundary=\"graphql\";subscriptionSpec=1.0,application/json",
+      ])
+    let response = HTTPResponse<MockSubscription<MockSelectionSet>>(
+      response: HTTPURLResponse(
+        url: TestURL.mockServer.url,
+        statusCode: 200,
+        httpVersion: nil,
+        headerFields: [
+          "Content-Type": "multipart/mixed;boundary=graphql",
+        ])!,
+      rawData: rawData.crlfFormattedData(),
+      parsedResponse: nil)
+
+    let expectedData = "{\"data\":{\"ticker\":1}}".data(using: .utf8)
+    let chain = DataCaptureRequestChain<MockSubscription<MockSelectionSet>> { data in
+      XCTAssertEqual(data, expectedData)
+    }
+
+    measure {
+      MultipartResponseParsingInterceptor().interceptAsync(
+        chain: chain,
+        request: request,
+        response: response
+      ) { _ in }
+    }
+  }
+
   // MARK - Helpers
 
   func loadResponse<Query: GraphQLQuery>(
