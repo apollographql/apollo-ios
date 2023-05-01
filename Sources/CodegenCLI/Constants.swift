27934e875a10e59fae496fcd297a2bf38a7adef5
Calvin Cestari <calvin.cestari@apollographql.com>
2023-05-01 11:10:57 -0700

    Updated version numbers

--- Sources/CodegenCLI/Constants.swift
+++ Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "1.1.2"
+  public static let CLIVersion: String = "1.1.3"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
