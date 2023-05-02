c39e8f67d3a1132f7e37b979c78d2ddc18f6dbca
Calvin Cestari <calvin.cestari@apollographql.com>
2023-05-02 14:56:44 -0700

    Re-add ignored file

---
+++ Tests/TestCodeGenConfigurations/Other-CustomTarget/AnimalKingdomAPI/AnimalKingdomAPI.h
@@ -0,0 +1,11 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomTargetProject.
+FOUNDATION_EXPORT double AnimalKingdomAPIVersionNumber;
+
+//! Project version string for CustomTargetProject.
+FOUNDATION_EXPORT const unsigned char AnimalKingdomAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomTargetProject/PublicHeader.h>
+
+
