36f56c5635417e2bbc33ff2d6cc1ea55614ba66f
Anthony Miller <anthonymdev@gmail.com>
2023-05-04 16:04:35 -0700

    WIP: CacheDataExecutionSource
    TODO: Cache Reference resolving

--- Sources/Apollo/ExecutionSources/CacheDataExecutionSource.swift
+++ Sources/Apollo/ExecutionSources/CacheDataExecutionSource.swift
@@ -20,16 +20,14 @@ struct CacheDataExecutionSource: GraphQLExecutionSource {
       switch value {
       case let scalar as ScalarType:
         return scalar
-      case let customScalar as CustomScalarType:
-        return customScalar._jsonValue as? ScalarType
       default: return nil
       }
     }
 
     func transform(_ value: AnyHashable) -> ObjectData? {
       switch value {
-      case let object as DataDict:
-        return ObjectData(_transformer: DataTransformer(), _rawData: object._data)
+      case let object as JSONObject:
+        return ObjectData(_transformer: DataTransformer(), _rawData: object)
       default: return nil
       }
     }
---
+++ Tests/ApolloTests/ExecutionSources/CacheDataExecutionSource_OpaqueObjectDataWrapper_Tests.swift
@@ -0,0 +1,81 @@
+import Foundation
+import XCTest
+import Nimble
+@testable import Apollo
+import ApolloAPI
+
+class CacheDataExecutionSource_OpaqueObjectDataWrapper_Tests: XCTestCase {
+
+  // MARK: - Helpers
+
+  func test__subscript__forScalarField_returnsValue() throws {
+    // given
+    let data = [
+      "name": "Luke Skywalker"
+    ]
+
+    let objectData = CacheDataExecutionSource.opaqueObjectDataWrapper(for: data)
+
+    // when
+    let actual = objectData["name"]
+
+    // then
+    expect(actual as? String).to(equal("Luke Skywalker"))
+  }
+
+  // MARK: Object Fields
+
+  func test__subscript__forObjectField_returnsValueAsObjectDataWrapper() throws {
+    // given
+    let data = [
+      "friend": [
+        "name": "Luke Skywalker"
+      ]
+    ]
+
+    let objectData = CacheDataExecutionSource.opaqueObjectDataWrapper(for: data)
+
+    // when
+    let actual = objectData["friend"]?["name"]
+
+    // then
+    expect(actual as? String).to(equal("Luke Skywalker"))
+  }
+
+  // MARK: List Fields
+
+  func test__subscript__forListOfScalarField_returnsValue() throws {
+    // given
+    let data = [
+      "list": ["Luke Skywalker"]
+    ]
+
+    let objectData = CacheDataExecutionSource.opaqueObjectDataWrapper(for: data)
+
+    // when
+    let actual = objectData["list"]?[0]
+
+    // then
+    expect(actual as? String).to(equal("Luke Skywalker"))
+  }
+
+  func test__subscript__forListOfObjectsField_returnsValueAsObjectDict() throws {
+    // given
+    let data = [
+      "friends": [
+        [
+          "name": "Luke Skywalker"
+        ]
+      ]
+    ]
+
+    let objectData = CacheDataExecutionSource.opaqueObjectDataWrapper(for: data)
+
+    // when
+    let actual = objectData["friends"]?[0]?["name"]
+
+    // then
+    expect(actual as? String).to(equal("Luke Skywalker"))
+  }
+
+}
