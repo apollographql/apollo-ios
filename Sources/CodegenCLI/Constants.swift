c7d618adc40bc20e4f40651b8debbc3c9f6d2b6f
Anthony Miller <anthonymdev@gmail.com>
2023-05-15 19:21:29 -0700

    Fix project version (1.2 -> 1.2.0)

--- Sources/CodegenCLI/Constants.swift
+++ Sources/CodegenCLI/Constants.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 public enum Constants {
-  public static let CLIVersion: String = "1.2"
+  public static let CLIVersion: String = "1.2.0"
   static let defaultFilePath: String = "./apollo-codegen-config.json"
 }
