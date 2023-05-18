df04613171b9063691ad9179753c6f0dd3fe2546
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-05-18 11:02:19 -0400

    Initial rework of URLSessionClientTests
    - Added `MockURLProtocol` to use with `URLSessionClientTests`
    - Updated test code to use new mock protocol
    - Moved `URLSessionClientTests` from Integration to Unit tests

---
+++ Tests/ApolloInternalTestHelpers/MockURLProtocol.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+public class MockURLProtocol: URLProtocol {
+  
+  public typealias MockRequestHandler = ((URLRequest) throws -> Result<(HTTPURLResponse, Data?), Error>)
+  
+  // Dictionary of mock request handlers where the `key` is the URL of the request.
+  static var requestHandlers = [URL: MockRequestHandler]()
+  
+  override class public func canInit(with request: URLRequest) -> Bool {
+    return true
+  }
+  
+  override class public func canonicalRequest(for request: URLRequest) -> URLRequest {
+    return request
+  }
+  
+  override public func startLoading() {
+    guard let url = request.url,
+          let handler = MockURLProtocol.requestHandlers[url] else {
+      fatalError("No URL available for URLRequest.")
+    }
+    
+    defer {
+      MockURLProtocol.requestHandlers.removeValue(forKey: url)
+    }
+    
+    do {
+      let result = try handler(request)
+      
+      switch result {
+      case let .success((response, data)):
+        client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        
+        if let data = data {
+          client?.urlProtocol(self, didLoad: data)
+        }
+        
+        client?.urlProtocolDidFinishLoading(self)
+      case let .failure(error):
+        client?.urlProtocol(self, didFailWithError: error)
+      }
+      
+    } catch {
+      client?.urlProtocol(self, didFailWithError: error)
+    }
+  }
+  
+  override public func stopLoading() {
+  }
+  
+}
---
+++ Tests/ApolloTests/Network/URLSessionClientTests.swift
@@ -0,0 +1,288 @@
+import XCTest
+@testable import Apollo
+
+class URLSessionClientTests: XCTestCase {
+  
+  var client: URLSessionClient!
+  var sessionConfiguration: URLSessionConfiguration!
+
+  override func setUp() {
+    super.setUp()
+
+    sessionConfiguration = URLSessionConfiguration.default
+    sessionConfiguration.protocolClasses = [MockURLProtocol.self]
+    client = URLSessionClient(sessionConfiguration: sessionConfiguration)
+  }
+
+  override func tearDown() {
+    client = nil
+
+    super.tearDown()
+  }
+  
+  private func request(
+    for url: URL,
+    responseData: Data?,
+    statusCode: Int,
+    httpVersion: String? = nil,
+    headerFields: [String: String]? = nil
+  ) -> URLRequest {
+    let request = URLRequest(url: url,
+                             cachePolicy: .reloadIgnoringCacheData,
+                             timeoutInterval: 10)
+    
+    MockURLProtocol.requestHandlers[url] = { request in
+      guard let requestURL = request.url else {
+        throw URLError(.badURL)
+      }
+      
+      let response = HTTPURLResponse(url: requestURL,
+                                     statusCode: statusCode,
+                                     httpVersion: httpVersion,
+                                     headerFields: headerFields)
+      return .success((response!, responseData))
+    }
+    
+    return request
+  }
+  
+  func testBasicGet() {
+    let url = URL(string: "http://www.test.com/basicget")!
+    let stringResponse = "Basic GET Response Data"
+    let request = self.request(for: url,
+                               responseData: stringResponse.data(using: .utf8),
+                               statusCode: 200)
+    let expectation = self.expectation(description: "Basic GET request completed")
+    self.client.sendRequest(request) { result in
+      defer {
+        expectation.fulfill()
+      }
+      
+      switch result {
+      case .failure(let error):
+        XCTFail("Unexpected error: \(error)")
+      case .success(let (data, httpResponse)):
+        XCTAssertFalse(data.isEmpty)
+        XCTAssertEqual(String(data: data, encoding: .utf8), stringResponse)
+        XCTAssertEqual(request.url, httpResponse.url)
+        XCTAssertEqual(httpResponse.statusCode, 200)
+      }
+    }
+    
+    self.wait(for: [expectation], timeout: 5)
+  }
+  
+  func testGettingImage() {
+    let url = URL(string: "http://www.test.com/gettingImage")!
+    #if os(macOS)
+    let responseImg = NSImage(systemSymbolName: "pencil", accessibilityDescription: nil)
+    let responseData = responseImg?.tiffRepresentation
+    #else
+    let responseImg = UIImage(systemName: "pencil")
+    let responseData = responseImg.pngData()
+    #endif
+    let headerFields = ["Content-Type": "image/jpeg"]
+    let request = self.request(for: url,
+                               responseData: responseData,
+                               statusCode: 200,
+                               headerFields: headerFields)
+    
+    let expectation = self.expectation(description: "GET request for image completed")
+    self.client.sendRequest(request) { result in
+      defer {
+        expectation.fulfill()
+      }
+
+      switch result {
+      case .failure(let error):
+        XCTFail("Unexpected error: \(error)")
+      case .success(let (data, httpResponse)):
+        XCTAssertFalse(data.isEmpty)
+        XCTAssertEqual(httpResponse.allHeaderFields["Content-Type"] as! String, "image/jpeg")
+        #if os(macOS)
+          let image = NSImage(data: data)
+          XCTAssertNotNil(image)
+        #else
+          let image = UIImage(data: data)
+          XCTAssertNotNil(image)
+        #endif
+        XCTAssertEqual(request.url, httpResponse.url)
+      }
+    }
+
+    self.wait(for: [expectation], timeout: 5)
+  }
+  
+  func testPostingJSON() throws {
+    let testJSON = ["key": "value"]
+    let data = try JSONSerialization.data(withJSONObject: testJSON, options: .prettyPrinted)
+    let url = URL(string: "http://www.test.com/postingJSON")!
+    let headerFields = ["Content-Type": "application/json"]
+    
+    var request = self.request(for: url,
+                               responseData: data,
+                               statusCode: 200,
+                               headerFields: headerFields)
+    request.httpBody = data
+    request.httpMethod = GraphQLHTTPMethod.POST.rawValue
+
+    let expectation = self.expectation(description: "POST request with JSON completed")
+    self.client.sendRequest(request) { result in
+      defer {
+        expectation.fulfill()
+      }
+
+      switch result {
+      case .failure(let error):
+        XCTFail("Unexpected error: \(error)")
+      case .success(let (data, httpResponse)):
+        XCTAssertEqual(request.url, httpResponse.url)
+
+        do {
+          let parsedJSON = try JSONSerialization.jsonObject(with: data) as! [String : String]
+          XCTAssertEqual(parsedJSON, testJSON)
+        } catch {
+          XCTFail("Unexpected error: \(error)")
+        }
+      }
+    }
+
+    self.wait(for: [expectation], timeout: 10)
+  }
+  
+  func testCancellingTaskDirectlyCallsCompletionWithError() throws {
+    let url = URL(string: "http://www.test.com/cancelTaskDirectly")!
+    let request = request(for: url,
+                          responseData: nil,
+                          statusCode: -1)
+
+    let expectation = self.expectation(description: "Cancelled task completed")
+    let task = self.client.sendRequest(request) { result in
+      defer {
+        expectation.fulfill()
+      }
+
+      switch result {
+      case .failure(let error):
+        switch error {
+        case URLSessionClient.URLSessionClientError.networkError(let data, let httpResponse, let underlying):
+          XCTAssertTrue(data.isEmpty)
+          XCTAssertNil(httpResponse)
+          let nsError = underlying as NSError
+          XCTAssertEqual(nsError.domain, NSURLErrorDomain)
+          XCTAssertEqual(nsError.code, NSURLErrorCancelled)
+        default:
+          XCTFail("Unexpected error: \(error)")
+        }
+      case .success:
+        XCTFail("Task succeeded when it should have been cancelled!")
+      }
+    }
+
+    task.cancel()
+
+    self.wait(for: [expectation], timeout: 10)
+  }
+  
+  func testCancellingTaskThroughClientDoesNotCallCompletion() throws {
+    let url = URL(string: "http://www.test.com/cancelThroughClient")!
+    let request = request(for: url,
+                          responseData: nil,
+                          statusCode: -1)
+
+    let expectation = self.expectation(description: "Cancelled task completed")
+    expectation.isInverted = true
+    let task = self.client.sendRequest(request) { result in
+      // This shouldn't get hit since we cancel the task immediately
+      expectation.fulfill()
+    }
+
+    self.client.cancel(task: task)
+
+    self.wait(for: [expectation], timeout: 5)
+
+  }
+  
+//  func testMultipleSimultaneousRequests() {
+//    let expectation = self.expectation(description: "request sent, response received")
+//    let iterations = 20
+//    expectation.expectedFulfillmentCount = iterations
+//    @Atomic var taskIDs: [Int] = []
+//
+//    DispatchQueue.concurrentPerform(iterations: iterations, execute: { index in
+//      let request = self.request(for: .getWithIndex(index: index))
+//
+//      let task = self.client.sendRequest(request) { result in
+//        switch result {
+//        case .success((let data, let response)):
+//          XCTAssertEqual(response.url, request.url)
+//          XCTAssertFalse(data.isEmpty)
+//          do {
+//            let httpBinResponse = try HTTPBinResponse(data: data)
+//            // WORKAROUND: HTTPBin started randomly returning http instead of https
+//            // in this field, replace until addressed. Issue: https://github.com/postmanlabs/httpbin/issues/615
+//            var responseURLString = httpBinResponse.url
+//            if !responseURLString.contains("https") {
+//              let droppedHTTP = String(responseURLString.dropFirst(4))
+//              responseURLString = "https" + droppedHTTP
+//            }
+//
+//            XCTAssertEqual(responseURLString, response.url?.absoluteString)
+//            XCTAssertEqual(httpBinResponse.args?["index"], "\(index)")
+//          } catch {
+//            XCTFail("Parsing error: \(error) for url \(response.url!)")
+//          }
+//        case .failure(let error):
+//          XCTFail("Unexpected error: \(error)")
+//        }
+//
+//        DispatchQueue.main.async {
+//          expectation.fulfill()
+//        }
+//      }
+//
+//      $taskIDs.mutate { $0.append(task.taskIdentifier) }
+//    })
+//
+//    self.wait(for: [expectation], timeout: 30)
+//
+//    // Were the correct number of tasks created?
+//    XCTAssertEqual(taskIDs.count, iterations)
+//
+//    // Using a set to unique, are all task IDs different values?)
+//    let set = Set(taskIDs)
+//    XCTAssertEqual(set.count, iterations)
+//  }
+  
+  func testInvalidatingClientAndThenTryingToSendARequestReturnsAppropriateError() {
+    let client = URLSessionClient(sessionConfiguration: sessionConfiguration)
+    client.invalidate()
+
+    let url = URL(string: "http://www.test.com/invalidatedRequestTest")!
+    let request = request(for: url,
+                          responseData: nil,
+                          statusCode: 400)
+    let expectation = self.expectation(description: "Basic GET request completed")
+    client.sendRequest(request) { result in
+      defer {
+        expectation.fulfill()
+      }
+
+      switch result {
+      case .failure(let error):
+        switch error {
+        case URLSessionClient.URLSessionClientError.sessionInvalidated:
+          // This is what we want
+          break
+        default:
+          XCTFail("Unexpected error: \(error)")
+        }
+      case .success:
+        XCTFail("This should not have succeeded")
+      }
+    }
+
+    self.wait(for: [expectation], timeout: 5)
+  }
+  
+}
