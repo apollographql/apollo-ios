76bb29886e3f05f8974f9bdf1e752eedfbeda877
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-06-07 13:57:26 -0400

    Schema cleanup

--- Tests/ApolloCodegenTests/Extensions/String+Data.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension String {
-  var asData: Data { self.data(using: .utf8)! }
-}
-
-extension Data {
-  var asString: String { String(data: self, encoding: .utf8)! }
-}
--- Tests/ApolloTests/VersionNumberTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import Apollo
-
-class VersionNumberTests: XCTestCase {
-  func testVersionNumberExists() {
-    // It would be the first 2 digits of version number like `0.19`.
-    XCTAssertGreaterThanOrEqual(ApolloVersionNumber, 0)
-  }
-}
