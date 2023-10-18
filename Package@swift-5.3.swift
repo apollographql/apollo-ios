3a1960d1b98319282ef7f8d3ebb6af445a40caa2
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-10-18 16:56:38 -0400

    Testing out swift 5.3 legacy unit test

---
+++ Package@swift-5.3.swift
@@ -0,0 +1,62 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+// NOTE: This Package.swift file targeting the Swift 5.3 toolchain is to facilitate support for testing on older iOS version
+// in GitHub actions and does not contain all products associated with the main Package.swift
+
+import PackageDescription
+
+let package = Package(
+  name: "Apollo",
+  platforms: [
+    .iOS(.v12),
+    .macOS(.v10_14),
+    .tvOS(.v12),
+    .watchOS(.v5)
+  ],
+  products: [
+    .library(name: "Apollo", targets: ["Apollo"]),
+    .library(name: "ApolloAPI", targets: ["ApolloAPI"]),
+    .library(name: "Apollo-Dynamic", type: .dynamic, targets: ["Apollo"]),
+    .library(name: "ApolloSQLite", targets: ["ApolloSQLite"]),
+    .library(name: "ApolloWebSocket", targets: ["ApolloWebSocket"]),
+    .library(name: "ApolloTestSupport", targets: ["ApolloTestSupport"])
+  ],
+  dependencies: [
+    .package(
+      url: "https://github.com/stephencelis/SQLite.swift.git",
+      .upToNextMajor(from: "0.13.1")),
+  ],
+  targets: [
+    .target(
+      name: "Apollo",
+      dependencies: [
+        "ApolloAPI"
+      ]
+    ),
+    .target(
+      name: "ApolloAPI",
+      dependencies: []
+    ),
+    .target(
+      name: "ApolloSQLite",
+      dependencies: [
+        "Apollo",
+        .product(name: "SQLite", package: "SQLite.swift"),
+      ]
+    ),
+    .target(
+      name: "ApolloWebSocket",
+      dependencies: [
+        "Apollo"
+      ]
+    ),
+    .target(
+      name: "ApolloTestSupport",
+      dependencies: [
+        "Apollo",
+        "ApolloAPI"
+      ]
+    )
+  ]
+)
