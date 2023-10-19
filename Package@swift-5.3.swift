2bfdea62282666d4496837677fa7198cea4fd6a2
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-10-19 11:19:26 -0400

    Trying to fix package resolution issue

--- Package@swift-5.3.swift
+++ Package@swift-5.3.swift
@@ -1,13 +1,10 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// NOTE: This Package.swift file targeting the Swift 5.3 toolchain is to facilitate support for testing on older iOS version
-// in GitHub actions and does not contain all products associated with the main Package.swift
-
 import PackageDescription
 
 let package = Package(
-  name: "Apollo",
+  name: "apollo-ios",
   platforms: [
     .iOS(.v12),
     .macOS(.v10_14),
@@ -17,7 +14,7 @@ let package = Package(
   products: [
     .library(name: "Apollo", targets: ["Apollo"]),
     .library(name: "ApolloAPI", targets: ["ApolloAPI"]),
-    .library(name: "Apollo-Dynamic", type: .dynamic, targets: ["Apollo"]),
+//    .library(name: "Apollo-Dynamic", type: .dynamic, targets: ["Apollo"]),
     .library(name: "ApolloSQLite", targets: ["ApolloSQLite"]),
     .library(name: "ApolloWebSocket", targets: ["ApolloWebSocket"]),
     .library(name: "ApolloTestSupport", targets: ["ApolloTestSupport"])
