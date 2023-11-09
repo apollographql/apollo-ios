37b4118e760221a93c687433fa8e84429ae82f38
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2023-11-09 15:58:11 -0500

    Adding download-cli script
    Adding download cli script and calling it from the InstallCLI plugin

--- Plugins/InstallCLI/InstallCLIPluginCommand.swift
+++ Plugins/InstallCLI/InstallCLIPluginCommand.swift
@@ -5,42 +5,17 @@ import PackagePlugin
 struct InstallCLIPluginCommand: CommandPlugin {
 
   func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
-    var apolloVersion: String? = nil
     let dependencies = context.package.dependencies
     try dependencies.forEach { dep in
       if dep.package.displayName == "Apollo" {
-//          debugPrint("Apollo package directory - \(dep.package.directory)")
-//          debugPrint("Apollo package id - \(dep.package.id)")
-//          debugPrint("Apollo package origin - \(dep.package.origin)")
-//          debugPrint("Apollo package toolsVersion - \(dep.package.toolsVersion)")
-          let process = Process()
-          let path = try context.tool(named: "sh").path
-          process.executableURL = URL(fileURLWithPath: path.string)
-          process.arguments = ["\(dep.package.directory)/scripts/get-version.sh"]
-          let outputPipe = Pipe()
-          process.standardOutput = outputPipe
-          try process.run()
-          process.waitUntilExit()
-          
-          let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
-          if let version = String(data: data, encoding: .utf8) {
-              apolloVersion = version.trimmingCharacters(in: .whitespacesAndNewlines)
-          }
+        let process = Process()
+        let path = try context.tool(named: "sh").path
+        process.executableURL = URL(fileURLWithPath: path.string)
+        process.arguments = ["\(dep.package.directory)/scripts/download-cli.sh", context.package.directory.string]
+        try process.run()
+        process.waitUntilExit()
       }
     }
-    
-    debugPrint("Apollo Version - \(apolloVersion)")
-      
-//    guard let apolloPath = apolloDirectoryPath else {
-//      fatalError("No Apollo dependency directory path")
-//    }
-      
-//    let process = Process()
-//    let tarPath = try context.tool(named: "tar").path
-//    process.executableURL = URL(fileURLWithPath: tarPath.string)
-//    process.arguments = ["-xvf", "\(apolloPath)/CLI/apollo-ios-cli.tar.gz"]
-//    try process.run()
-//    process.waitUntilExit()
   }
 
   func createSymbolicLink(from: PackagePlugin.Path, to: PackagePlugin.Path) throws {
@@ -65,9 +40,9 @@ extension InstallCLIPluginCommand: XcodeCommandPlugin {
     print("Installing Apollo CLI Plugin to Xcode project \(context.xcodeProject.displayName)")
     let apolloPath = "\(context.pluginWorkDirectory)/../../checkouts/apollo-ios"
     let process = Process()
-    let tarPath = try context.tool(named: "tar").path
-    process.executableURL = URL(fileURLWithPath: tarPath.string)
-    process.arguments = ["-xvf", "\(apolloPath)/CLI/apollo-ios-cli.tar.gz"]
+    let path = try context.tool(named: "sh").path
+    process.executableURL = URL(fileURLWithPath: path.string)
+    process.arguments = ["\(apolloPath)/scripts/download-cli.sh", context.xcodeProject.directory.string]
     try process.run()
     process.waitUntilExit()
   }
