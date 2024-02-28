4b3384e7f5aba21073b92af77030c9d347f2e054
Zach FettersMoore <4425109+BobaFetters@users.noreply.github.com>
2024-02-28 12:54:29 -0500

    CLI download update

--- scripts/download-cli.sh
+++ scripts/download-cli.sh
@@ -15,5 +15,5 @@ APOLLO_VERSION=$(sh "$directory/get-version.sh")
 DOWNLOAD_URL="https://www.github.com/apollographql/apollo-ios/releases/download/$APOLLO_VERSION/apollo-ios-cli.tar.gz"
 FILE_PATH="$projectDir/apollo-ios-cli.tar.gz"
 curl -L "$DOWNLOAD_URL" -s -o "$FILE_PATH"
-tar -xvf "$FILE_PATH"
-rm -f "$FILE_PATH"
+#tar -xvf "$FILE_PATH"
+#rm -f "$FILE_PATH"
