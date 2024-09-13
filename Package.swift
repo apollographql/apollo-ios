898597bbb2e86bce96e4759323601054776e6361
Calvin Cestari <calvincestari@users.noreply.github.com>
2024-09-13 13:36:34 -0700

    chore: Required swift-tools-version update (apollographql/apollo-ios-dev#479)

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 // swift-tools-version:5.9
+//
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+// Swift 5.9 is available from Xcode 15.0.
+
 
 import PackageDescription
 
