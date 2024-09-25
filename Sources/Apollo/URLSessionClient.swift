b8937b20358e3b12b18789a0769107d37baa9474
Åsmund Grammeltvedt <asmundg@big-oil.org>
2024-09-25 16:50:40 +0200

    feat: Fix multipart boundary predicate
    Only looking for "---" is insufficient to determine the multipart boundary, as the string can occur within JSON objects.
    
    As specified in https://datatracker.ietf.org/doc/html/rfc2046#section-5.1, the delimiter should be _on a separate line_, which means we can recognize it by the newline control characters that won't occur inside JSON objects.

--- Sources/Apollo/URLSessionClient.swift
+++ Sources/Apollo/URLSessionClient.swift
@@ -298,7 +298,7 @@ open class URLSessionClient: NSObject, URLSessionDelegate, URLSessionTaskDelegat
         return
       }
 
-      let boundaryMarker = "--\(boundaryString)"
+      let boundaryMarker = "\r\n--\(boundaryString)\r\n"
       guard
         let dataString = String(data: taskData.data, encoding: .utf8)?.trimmingCharacters(in: .newlines),
         let lastBoundaryIndex = dataString.range(of: boundaryMarker, options: .backwards)?.upperBound,
