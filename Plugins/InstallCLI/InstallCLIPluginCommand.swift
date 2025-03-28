4294a65b9ecacc26774e7f5c4ead531d38245e3d
Calvin Cestari <146856+calvincestari@users.noreply.github.com>
2025-03-28 11:49:22 -0700

    fix: Xcode 16.3 CLI installation (apollographql/apollo-ios-dev#623)

--- Plugins/InstallCLI/InstallCLIPluginCommand.swift
+++ Plugins/InstallCLI/InstallCLIPluginCommand.swift
@@ -1,8 +1,13 @@
 import Foundation
 import PackagePlugin
+import os
 
 @main
 struct InstallCLIPluginCommand: CommandPlugin {
+
+  enum Error: Swift.Error {
+    case CannotDetermineXcodeVersion
+  }
 
   func performCommand(context: PackagePlugin.PluginContext, arguments: [String]) async throws {
     let dependencies = context.package.dependencies
@@ -27,14 +32,69 @@ extension InstallCLIPluginCommand: XcodeCommandPlugin {
 
   /// 👇 This entry point is called when operating on an Xcode project.
   func performCommand(context: XcodePluginContext, arguments: [String]) throws {
-    print("Installing Apollo CLI Plugin to Xcode project \(context.xcodeProject.displayName)")
-    let apolloPath = "\(context.pluginWorkDirectory)/../../checkouts/apollo-ios"
     let process = Process()
-    let path = try context.tool(named: "sh").path
-    process.executableURL = URL(fileURLWithPath: path.string)
-    process.arguments = ["\(apolloPath)/scripts/download-cli.sh", context.xcodeProject.directory.string]
+    let toolPath = try context.tool(named: "sh").path
+    process.executableURL = URL(fileURLWithPath: toolPath.string)
+
+    let downloadScriptPath = try downloadScriptPath(context: context)
+    process.arguments = [downloadScriptPath, context.xcodeProject.directory.string]
+
     try process.run()
     process.waitUntilExit()
+  }
+
+  /// Used to get the location of the CLI download script.
+  ///
+  /// - Parameter context: Contextual information based on the plugin's stated intent and requirements.
+  /// - Returns: The path to the download script used to fetch the CLI binary.
+  private func downloadScriptPath(context: XcodePluginContext) throws -> String {
+    let xcodeVersion = try xcodeVersion(context: context)
+    let relativeScriptPath = "SourcePackages/checkouts/apollo-ios/scripts/download-cli.sh"
+    let absoluteScriptPath: String
+
+    if xcodeVersion.lexicographicallyPrecedes("16.3") {
+      absoluteScriptPath = "\(context.pluginWorkDirectory)/../../../\(relativeScriptPath)"
+    } else {
+      absoluteScriptPath = "\(context.pluginWorkDirectory)/../../../../\(relativeScriptPath)"
+    }
+
+    return absoluteScriptPath
+  }
+
+  /// Used to get a string representation of Xcode in the current toolchain.
+  ///
+  /// - Parameter context: Contextual information based on the plugin's stated intent and requirements.
+  /// - Returns: A string representation of the Xcode version.
+  private func xcodeVersion(context: XcodePluginContext) throws -> String {
+    let process = Process()
+    let toolPath = try context.tool(named: "xcrun").path
+    process.executableURL = URL(fileURLWithPath: toolPath.string)
+    process.arguments = ["xcodebuild", "-version"]
+
+    let outputPipe = Pipe()
+    process.standardOutput = outputPipe
+
+    try process.run()
+    process.waitUntilExit()
+
+    guard
+      let outputData = try outputPipe.fileHandleForReading.readToEnd(),
+      let output = String(data: outputData, encoding: .utf8)
+    else {
+      throw Error.CannotDetermineXcodeVersion
+    }
+
+    let xcodeVersionString = output.components(separatedBy: "\n")[0]
+    guard !xcodeVersionString.isEmpty else {
+      throw Error.CannotDetermineXcodeVersion
+    }
+
+    let versionString = xcodeVersionString
+      .components(separatedBy: CharacterSet.decimalDigits.inverted)
+      .compactMap({ $0.isEmpty ? nil : $0 })
+      .joined(separator: ".")
+
+    return versionString
   }
 
 }
