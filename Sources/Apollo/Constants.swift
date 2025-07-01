ca441856851e9ab7765387341dba76c0384f5991
Anthony Miller <anthonymdev@gmail.com>
2025-07-01 14:25:37 -0700

    Updated version numbers

--- Sources/Apollo/Constants.swift
+++ Sources/Apollo/Constants.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public enum Constants {
   public static let ApolloClientName = "apollo-ios"
-  public static let ApolloClientVersion: String = "1.23.0"
+  public static let ApolloClientVersion: String = "2.0.0-alpha-1"
 
   @available(*, deprecated, renamed: "ApolloClientVersion")
   public static let ApolloVersion: String = ApolloClientVersion
