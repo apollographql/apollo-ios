ffde16dea1cb78cd9923aca1a3954afc16c3068d
gh-action-runner <runner@iad20-eo1213-64403b58-81f1-4c7b-9248-dd5bc08fb363-3E3818FC7D58.local>
2026-04-23 17:25:25 +0000

    Updated version numbers

--- Sources/Apollo/Constants.swift
+++ Sources/Apollo/Constants.swift
@@ -2,5 +2,5 @@ import Foundation
 
 public enum Constants {
   public static let ApolloClientName = "apollo-ios"
-  public static let ApolloClientVersion: String = "2.1.0"
+  public static let ApolloClientVersion: String = "2.1.1"
 }
